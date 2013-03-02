5718dda95b56d3a9879e7f1e98e7305585f6de29
Ilja Kamps <ikarus@eris>
2013-03-02 04:13:20 +0100

    Longer blocks

--- application/static/js/custom.js
+++ application/static/js/custom.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('div.expander').expander({
-		slicePoint: 900, 
+		slicePoint: 1100, 
 		expandText: "lees meer",
 		userCollapseText: "lees minder",
 		expandSpeed: 1000,
