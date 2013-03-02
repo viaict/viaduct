022381488dac992226f1bab3a391fd6278c288cb
Ilja Kamps <ikarus@eris>
2013-03-02 01:53:10 +0100

    this I broke ?

--- application/views.py
+++ application/views.py
@@ -3,11 +3,15 @@ from flask import Markup
 from flask.ext.login import current_user
 
 from application import application
-from application.page.views import view_page
+from application.page.views import retrieve_page
 
 @application.route('/')
 def index():
-	return view_page()
+	# Hardcoded behavior for today, 4 subblock
+	#blocks = [ retrieve_page('index/' + str(i))[0] for i in range(1,5) ]
+
+	#return render_template('index.htm', revision=blocks)
+
 #def test():
 #	return 'testing'
 
