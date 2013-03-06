e6901cc66d27dbdfdb903b7a464c18d02825955b
S.J.R. van Schaik <stephan@synkhronix.com>
2013-03-06 14:22:45 +0000

    Refactoring the code base.

--- application/page/forms.py
+++
@@ -1,8 +0,0 @@
-from flask.ext.wtf import Form, TextField, SubmitField
-from flask.ext.wtf import Required, Regexp
-
-class ChangePathForm(Form):
-	path = TextField('Path', [Required(), Regexp(r'^ */?[\w-]+(/[\w-]+)*/? *$', message='You suck at typing URL paths')])
-	move_only_this = SubmitField('Only this page')
-	move_children = SubmitField('This and its children ')
-
--- application/static/js/custom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$('div.expander').expander({
-		slicePoint: 1100, 
-		expandText: "lees meer",
-		userCollapseText: "lees minder",
-		expandSpeed: 1000,
-		collapseSpeed: 1000,
-	});
-});
