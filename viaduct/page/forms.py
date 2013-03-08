448e0f92d07e7ad09ca9d2b922e3d1ab3e6063dc
S.J.R. van Schaik <stephan@synkhronix.com>
2013-03-08 18:58:27 +0000

    Working on edit page.

--- viaduct/page/forms.py
+++ viaduct/page/forms.py
@@ -1,5 +1,14 @@
-from flask.ext.wtf import Form, TextField, SubmitField
-from flask.ext.wtf import Required, Regexp
+from flask.ext.wtf import Form, BooleanField, TextField, TextAreaField
+from flask.ext.wtf import SelectField, SubmitField
+from flask.ext.wtf import Optional, NumberRange, Required, Regexp
+
+class EditPageForm(Form):
+	title = TextField('Title', [Required()])
+	content_type = SelectField('Content Type')
+	content = TextAreaField('Content', [Optional()])
+	priority = TextField('Priority', [NumberRange()])
+	save_page = SubmitField('Save Page')
+
 
 class ChangePathForm(Form):
 	path = TextField('Path', [Required(), Regexp(r'^ */?[\w-]+(/[\w-]+)*/? *$', message='You suck at typing URL paths')])
