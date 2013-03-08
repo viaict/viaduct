692cee76ede579e11369ee62a83de618db235b44
S.J.R. van Schaik <stephan@synkhronix.com>
2013-03-08 17:16:52 +0000

    Updated the database script.

--- create_db.py
+++ create_db.py
@@ -8,11 +8,6 @@ from viaduct.page.models import Page, PagePermission
 # Create the database.
 db.create_all()
 
-page = Page('')
-
-db.session.add(page)
-db.session.commit()
-
 # Add the anonymous user.
 user = User('anonymous', '', 'Anonymous', '')
 
@@ -22,11 +17,6 @@ db.session.commit()
 group = Group('anonymous')
 
 db.session.add(group)
-db.session.commit()
-
-permissions = PagePermission(group, page, view=True)
-
-db.session.add(permissions)
 db.session.commit()
 
 # Add the administrator.
