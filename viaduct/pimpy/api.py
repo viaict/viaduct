e30ca1cbc4b5fdfb61d91401dad3304143fca9cd
Maarten Inja <maarten.inja@gmail.com>
2013-03-13 19:51:35 +0100

    well.. got something of the api to work

--- viaduct/pimpy/api.py
+++ viaduct/pimpy/api.py
@@ -1,6 +1,20 @@
-from flask import render_template, request
+from flask import render_template, request, Markup
 
 from viaduct.pimpy.models import Task, Minute
+from viaduct.page.models import Page
+from flask.ext.login import current_user
 
 class PimpyAPI:
-	def get_group_list(group id):
+	@staticmethod
+	def get_navigation_menu():
+		print "api called"
+		path = request.path
+		page = Page.query.filter(Page.path==path).first()
+		groups = current_user.groups.all()
+
+		current_group = 'administrators'
+		#request.args.get('group', '') =
+
+		return Markup(render_template('pimpy/side_menu.htm',
+			page=page, path=path, groups=groups, current_group=current_group))
+
--- viaduct/pimpy/views.py
+++ viaduct/pimpy/views.py
@@ -8,21 +8,20 @@ from viaduct.helpers import flash_form_errors
 module = Blueprint('pimpy', __name__)
 
 @module.route('/pimpy/', methods=['GET', 'POST'])
-@module.route('/pimpy/<minutesOrTasks>/<groups>', methods=['GET', 'POST'])
 def view_page(minutesOrTasks='all', groups=""):
 
 	# haal ofwel de minutes ofwel de tasks op
 	list_items = []
-	for group in current_user.groups.all():
-		print "group id %d" % group.id
-		if request.args.get('minutesOrTasks', '') == 'tasks':
-			print "shall load tasks!" 
-			list_items.extend(group.tasks.all())
-		elif request.args.get('minutesOrTasks', '') == 'minutes':
-			print "shall load minutes!" 
-			list_items.extend(group.minutes.all())
-			
+	#for group in current_user.groups.all():
+	#	print "group id %d" % group.id
+	#	if request.args.get('minutesOrTasks', '') == 'tasks':
+	#		print "shall load tasks!" 
+	#		list_items.extend(group.tasks.all())
+	#	elif request.args.get('minutesOrTasks', '') == 'minutes':
+	#		print "shall load minutes!" 
+	#		list_items.extend(group.minutes.all())
+	#		
 
-	print len(list_items)
+	#print len(list_items)
 	return render_template('pimpy/view_page.htm', list_items=list_items, page=True)
 
