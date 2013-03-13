e91e9bae72db4296f0188293a97e5cb889fae92b
Maarten Inja <maarten.inja@gmail.com>
2013-03-13 19:51:35 +0100

    well.. got something of the api to work

--- viaduct/pimpy/__init__.py
+++ viaduct/pimpy/__init__.py
@@ -1,7 +1,9 @@
 from viaduct import application
 
 from views import module
+from api import PimpyAPI
 
 application.register_blueprint(module)
+application.jinja_env.globals.update(PimpyAPI=PimpyAPI)
 
 
