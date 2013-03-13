90a2fcb731c8938648844350ab84481b3f20bd8a
Maarten Inja <maarten.inja@gmail.com>
2013-03-13 20:54:55 +0100

    got the menu with type AND groups, current_group to work\!\!

--- viaduct/pimpy/api.py
+++ viaduct/pimpy/api.py
@@ -7,14 +7,32 @@ from flask.ext.login import current_user
 class PimpyAPI:
 	@staticmethod
 	def get_navigation_menu():
-		print "api called"
-		path = request.path
-		page = Page.query.filter(Page.path==path).first()
 		groups = current_user.groups.all()
-
 		current_group = 'administrators'
-		#request.args.get('group', '') =
+		group = request.args.get('group', '')
+		type = request.args.get('type', '')
 
-		return Markup(render_template('pimpy/side_menu.htm',
-			page=page, path=path, groups=groups, current_group=current_group))
+		return Markup(render_template('pimpy/api/side_menu.htm',
+			groups=groups, current_group=current_group, type=type))
 
+	@staticmethod
+	def get_minutes_or_tasks():
+		groups = current_user.groups.all()
+		current_group = 'administrators'
+		group = request.args.get('group', '')
+		type = request.args.get('type', '')
+
+
+		list_items = []
+		for group in current_user.groups.all():
+			print "group id %d" % group.id
+			if request.args.get('type', '') == 'tasks':
+				print "shall load tasks!" 
+				list_items.extend(group.tasks.all())
+			elif request.args.get('type', '') == 'minutes':
+				print "shall load minutes!" 
+				list_items.extend(group.minutes.all())
+
+		url_part = "tasks" if request.args.get('minutesOrTasks', '') == 'tasks' else "minutes"
+		return Markup(render_template('pimpy/api/%s.htm' % url_part,
+				list_items=list_items, groups=groups, current_group=current_group, type=type))
