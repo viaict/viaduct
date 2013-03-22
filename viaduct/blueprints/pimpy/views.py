91b346dbd8dbf230c5cc6226f6e47fb755cf705b
Maarten Inja <maarten.inja@gmail.com>
2013-03-22 18:41:22 +0100

    pimpy working with less get variables, but it is a bit messy

--- viaduct/blueprints/pimpy/views.py
+++ viaduct/blueprints/pimpy/views.py
@@ -5,32 +5,25 @@ from flask.ext.login import current_user
 from viaduct import application, db
 from viaduct.helpers import flash_form_errors
 
-#<<<<<<< HEAD:viaduct/blueprints/pimpy/views.py
-#blueprint = Blueprint('pimpy', __name__)
-#
-#@blueprint.route('/pimpy/', methods=['GET', 'POST'])
-#def view_page(minutesOrTasks='all', groups=""):
-#	return render_template('pimpy/view_page.htm')
-#=======
+
 from api import PimpyAPI
 
-module = Blueprint('pimpy', __name__)
+blueprint = Blueprint('pimpy', __name__)
 
-@module.route('/pimpy/', methods=['GET', 'POST'])
+@blueprint.route('/pimpy/', methods=['GET', 'POST'])
 def view_page():
 	return render_template('pimpy/view_page.htm', personal=True,
 	group_id='all', type='minutes')
 
-@module.route('/pimpy/minutes/', methods=['GET', 'POST'])
-@module.route('/pimpy/minutes/<int:group_id>', methods=['GET', 'POST'])
+@blueprint.route('/pimpy/minutes/', methods=['GET', 'POST'])
+@blueprint.route('/pimpy/minutes/<group_id>', methods=['GET', 'POST'])
 def view_minutes(group_id='all'):
 	return PimpyAPI.get_minutes(group_id)
 
-@module.route('/pimpy/tasks/', methods=['GET', 'POST'])
-@module.route('/pimpy/tasks/<int:group_id>', methods=['GET', 'POST'])
-@module.route('/pimpy/tasks/me', methods=['GET', 'POST'], defaults={'personal': True})
-@module.route('/pimpy/tasks/me/<int:group_id>', methods=['GET', 'POST'], defaults={'personal': True})
+@blueprint.route('/pimpy/tasks/', methods=['GET', 'POST'])
+@blueprint.route('/pimpy/tasks/<group_id>', methods=['GET', 'POST'])
+@blueprint.route('/pimpy/tasks/me', methods=['GET', 'POST'], defaults={'personal': True})
+@blueprint.route('/pimpy/tasks/me/<group_id>', methods=['GET', 'POST'], defaults={'personal': True})
 def view_tasks(group_id='all', personal=False):
 	return PimpyAPI.get_tasks(group_id, personal)
-#>>>>>>> should commit to be sure?:viaduct/pimpy/views.py
 
