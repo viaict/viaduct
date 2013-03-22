4d5070e6e0ec46e0bd244083db091ec850b47a59
Fabien Tesselaar <fabientesselaar@gmail.com>
2013-03-22 18:13:46 +0100

    Adjusted the routes

--- viaduct/blueprints/activity/views.py
+++ viaduct/blueprints/activity/views.py
@@ -1,10 +1,11 @@
-from flask import Blueprint
-from flask import abort, flash, redirect, render_template, request, url_for
+from flask import flash, get_flashed_messages, redirect, render_template, \
+	request, url_for, abort
+from flask import Blueprint, Markup
 from flask.ext.login import current_user
 
 from viaduct import application, db
 from viaduct.helpers import flash_form_errors
-
+from forms import CreateForm
 from models import Activity
 
 blueprint = Blueprint('activity', __name__)
@@ -13,11 +14,12 @@ blueprint = Blueprint('activity', __name__)
 @blueprint.route('/activities/', methods=['GET', 'POST'])
 @blueprint.route('/activities/<int:page>/', methods=['GET', 'POST'])
 def view(page=1):
+	print "hello world"
 	activities = Activity.query.paginate(page, 15, False)
 
 	return render_template('activity/view.htm', activities=activity)
 
-@blueprint.route('/create_activity/', methods=['GET', 'POST'])
+@blueprint.route('activity/create/', methods=['GET', 'POST'])
 def create():
 	form = CreateForm(request.form)
 
