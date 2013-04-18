c46efda7740725a0cfda0f00dcb274bc29d5bd29
Fabien Tesselaar <fabientesselaar@gmail.com>
2013-04-18 14:47:58 +0200

    Study en education samengevoegd -> was het zelfde

--- viaduct/blueprints/education/views.py
+++ viaduct/blueprints/education/views.py
@@ -1,8 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, flash, redirect, render_template, request, url_for
 
-blueprint = Blueprint('education', __name__)
+from application import db
 
-@blueprint.route('/examination')
-def view_examination():
-	return 'Hello'
+education = Blueprint('education', __name__)
 
+@education.route('/education/', methods=['GET', 'POST'])
+@education.route('/education/<int:page>/', methods=['GET', 'POST'])
+def view(page=1):
+
+	education = education.query.paginate(page, 15, False)
+
+	return render_template('education/view.htm', education=education)
+
+@education.route('/education/create/', methods=['GET', 'POST'])
+def create():
+	if request.method == 'POST':
+		title = request.form['title'].strip()
+		description = request.form['description'].strip()
+		type = request.form['type']
+		
+		valid_form = True
+
+		if valid_form:
+			education = education(title, description, type)
+
+			db.session.add(education)
+			db.session.commit()
+
+			flash('The education has been added.', 'success')
+
+			return redirect(url_for('education.view'))
+
+	return render_template('education/create.htm')
