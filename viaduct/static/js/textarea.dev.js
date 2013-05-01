38abe83bdc2f1ec983a9b3da87d1d283b2f71547
Fabien Tesselaar <fabientesselaar@gmail.com>
2013-05-01 21:23:37 +0200

    Textarea resizer (met throttle.js)

--- viaduct/static/js/textarea.dev.js
+++ viaduct/static/js/textarea.dev.js
@@ -1,30 +1,19 @@
 // Licensed "AS IF" by Fabien Tesselaar (June 7, 2012)
 // Automatic textarea resizing
 //  - Dependency = throttle (and assumes twitter bootstrap css)
-$.fn.textarea = function() {
+$.fn.flexible = function() {  
   return this.each(function() {
     var holder = $('<div class="span6"></div>')
-      .css({'display':'none', 'text-align':'justify'})
+      .css({'display':'none'})
       .appendTo('body');
-      
+ 
     $(this)
-      .after('<span id="scroll_me"><span>')
-      .css('overflow', 'hidden')
-      .keyup($.throttle(250, resize));
+      .keyup($.throttle(40, resize))
+      .resize(); // resize on startup
     
     function resize() {
-      holder.html(
-        this.value
-          .replace(/</g, '&lt;')
-          .replace(/\n/g, '<br>')
-      );
-
-      var current = $(this).height();
-      var real    = holder.height()+5;
-      
-      if (current < real || real-20 < current)
-        $(this).css('height', real).stop(true, true).animate({height:real+10}, 140);
+      holder.html( '<br>' + this.value.replace(/</g, '&lt;').replace(/\n/g, '<br>') + '<br>');
+      $(this).css('height', holder.height());
     }
-    
   });
 };
