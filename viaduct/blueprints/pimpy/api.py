30f92f257ed2c89f9343db39713a41e625bc591f
Maarten Inja <maarten.inja@gmail.com>
2013-05-01 16:50:16 +0200

    pimpy stuff added

--- viaduct/blueprints/pimpy/api.py
+++ viaduct/blueprints/pimpy/api.py
@@ -19,6 +19,7 @@ class PimpyAPI:
 		endpoint = 'pimpy.view_' + type
 		endpoints = {'view_chosentype' : endpoint,
 					'view_chosentype_personal' : endpoint + '_personal',
+					'view_chosentype_chosenpersonal' : endpoint + (('_personal' if personal else '') if type != 'minutes' else ''),
 					'view_tasks' : 'pimpy.view_tasks',
 					'view_tasks_personal' : 'pimpy.view_tasks_personal',
 					'view_tasks_chosenpersonal' : 'pimpy.view_tasks',
@@ -33,18 +34,42 @@ class PimpyAPI:
 
 	@staticmethod
 	def get_tasks(group_id, personal):
+		# TODO: only return tasks with status != completed
 
 		list_items = []
 
+
 		if personal:
 			query = current_user.tasks
-			if group_id != 'all':
+			if group_id == 'all':
+				list_items.extend(query.all())
+			else:
 				query = query.filter(Task.group_id==group_id)
-			list_items.extend(query.all())
+				list_items.extend(query.all())
 		else:
-			groups = current_user.groups.all()
-			for group in groups:
-				list_items.extend(group.tasks.all())
+			if group_id == 'all':
+				groups = current_user.groups
+				for group in groups:
+					list_items.extend(group.tasks.all())
+			else:
+				query = current_user.tasks
+				query = query.filter(Task.group_id==group_id)
+				list_items.extend(query.all())
+
+		#if personal:
+		#	query = current_user.tasks
+		#	if group_id != 'all':
+		#		query = query.filter(Task.group_id==group_id)
+		#	list_items.extend(query.all())
+		#else:
+		#	groups = current_user.groups
+		#	if group_id == 'all':
+		#		groups = groups.all()
+		#		for group in groups:
+		#			list_items.extend(group.tasks.all())
+		#	else:
+		#		groups.filter(Task.group_id==group_id)
+		#		list_items.extend(groups.all())
 
 		return Markup(render_template('pimpy/api/tasks.htm',
 			list_items=list_items, type='tasks', group_id=group_id,
@@ -59,6 +84,6 @@ class PimpyAPI:
 			query = query.filter(Minute.group_id==group_id)
 		list_items.extend(query.all())
 
-		return Markup(render_template('pimpy/api/tasks.htm',
+		return Markup(render_template('pimpy/api/minutes.htm',
 			list_items=list_items, type='minutes', group_id=group_id))
 
