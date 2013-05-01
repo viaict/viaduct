d778babe914a0ceb6e4717238226763335446fb2
fabien <fabientesselaar@gmail.com>
2013-05-01 23:02:28 +0200

    sfasf

--- viaduct/blueprints/activity/forms.py
+++ viaduct/blueprints/activity/forms.py
@@ -2,10 +2,11 @@ from flask.ext.wtf import Form, TextField, TextAreaField, FileField, DateTimeFie
 
 class CreateForm(Form):
 	name				= TextField(u'Activity name', validators=[Required()])
-	description = TextAreaField(u'Description')
+	description = TextAreaField(u'Description', validators=[Required()])
 	start_time  = DateTimeField(u'Start time', format='%Y-%m-%d %H:%M:%S') 
 	end_time		= DateTimeField(u'End time', format='%Y-%m-%d %H:%M:%S')
 	location    = TextField(u'Location', default="VIA Kamer (A0.10)")
 	privacy			= TextField(u'Privacy')
+	price				= TextField(u'Price')
 	picture			= FileField(u'Picture')
 	venue				= TextField(u'Venue')
--- viaduct/blueprints/activity/models.py
+++ viaduct/blueprints/activity/models.py
@@ -13,16 +13,14 @@ class Activity(db.Model):
 	end_time			= db.Column(db.DateTime)
 	location			= db.Column(db.String(64))
 	privacy				= db.Column(db.String(64))
+	price					= db.Column(db.String(16))
 	picture				= db.Column(db.String(255))
 	venue					= db.Column(db.Integer) # venue ID
 	updated_time	= db.Column(db.DateTime, default=datetime.datetime.utcnow())
 	
 	owner = db.relationship('User', backref=db.backref('activities', lazy='dynamic'))
 
-	# Automatically pick dates that make sense
-	# Picture url to via_event logo
-	# Default location to via kamer
-	def __init__(self, owner_id, name, description, start_time, end_time, location, privacy, picture, venue):
+	def __init__(self, owner_id, name, description, start_time, end_time, location, privacy, price, picture, venue):
 		self.owner_id = owner_id
 		self.name = name
 		self.description = description
@@ -30,6 +28,15 @@ class Activity(db.Model):
 		self.end_time = end_time
 		self.location = location
 		self.privacy = privacy
+		self.price = price
 		self.picture = picture
 		self.venue = 1
+
+	def get_time(self):
+		if self.start_time.month == self.end_time.month:
+			if self.start_time.day == self.end_time.day:
+				return self.start_time.strftime("%A %d %b, %H:%M - ") + self.end_time.strftime("%H:%M") 
+			else:
+				return self.start_time.strftime("%a. %d %b (%H:%M) - ")  + \
+					self.end_time.strftime("%a. %d (%H:%M) %b") 
 
