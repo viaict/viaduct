50166ae07b938ee3c6f5eee16afd9ba3f5216d85
fabien <fabientesselaar@gmail.com>
2013-05-01 20:32:50 +0200

    Activity updated

--- viaduct/blueprints/activity/forms.py
+++ viaduct/blueprints/activity/forms.py
@@ -1,6 +1,11 @@
-from flask.ext.wtf import Form, TextField
-from flask.ext.wtf import Required, Email, EqualTo
+from flask.ext.wtf import Form, TextField, TextAreaField, FileField, DateTimeField, Required
 
 class CreateForm(Form):
-	title = TextField('title', validators=[Required()])
-	description = TextField('description')
+	name				= TextField(u'Activity name', validators=[Required()])
+	description = TextAreaField(u'Description')
+	start_time  = DateTimeField(u'Start time', format='%Y-%m-%d %H:%M:%S') 
+	end_time		= DateTimeField(u'End time', format='%Y-%m-%d %H:%M:%S')
+	location    = TextField(u'Location', default="VIA Kamer (A0.10)")
+	privacy			= TextField(u'Privacy')
+	picture			= FileField(u'Picture')
+	venue				= TextField(u'Venue')
