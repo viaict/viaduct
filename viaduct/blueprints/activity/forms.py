366ef42e9cb0f22550343737663c9aadb323c1ef
fabien <fabientesselaar@gmail.com>
2013-05-03 14:15:53 +0200

    Single activity view update + firefox fix voor google map

--- viaduct/blueprints/activity/forms.py
+++ viaduct/blueprints/activity/forms.py
@@ -1,11 +1,13 @@
-from flask.ext.wtf import Form, TextField, TextAreaField, FileField, DateTimeField, Required
+from flask.ext.wtf import Form, TextField, TextAreaField, FileField, Required
 
 class CreateForm(Form):
 	name				= TextField(u'Activity name', validators=[Required()])
-	description = TextAreaField(u'Description', validators=[Required()])
-	start_time  = DateTimeField(u'Start time', format='%Y-%m-%d %H:%M:%S') 
-	end_time		= DateTimeField(u'End time', format='%Y-%m-%d %H:%M:%S')
-	location    = TextField(u'Location', default="VIA Kamer (A0.10)")
+	description	= TextAreaField(u'Description', validators=[Required()])
+	start_date	= TextField(u'start_time', validators=[Required()]) 
+	start_time	= TextField(u'start_time', validators=[Required()]) 
+	end_date		= TextField(u'Venue')
+	end_time		= TextField(u'Venue')
+	location		= TextField(u'Location', default="VIA Kamer (A0.10)")
 	privacy			= TextField(u'Privacy')
 	price				= TextField(u'Price')
 	picture			= FileField(u'Picture')
