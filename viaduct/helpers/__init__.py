ff619dc715791fde96052ec50ad65eae7e03f368
Maarten Inja <maarten.inja@gmail.com>
2013-05-26 04:39:53 +0200

    activiteitenovericht op main page beter

--- viaduct/helpers/__init__.py
+++ viaduct/helpers/__init__.py
@@ -51,17 +51,19 @@ def pages_filter(data):
 				path=data[i].path) + '"><i class="icon-pencil"></i> Edit Page</a>'
 			content += '</div>'
 
-		content += '<h1>{0}</h1>'.format(data[i].title)
 
 		if i == 1 and data[i].filter_html:
+			content += '<h1>{0}</h1>'.format(data[i].title)
 			content += markdown(data[i].content, safe_mode='escape',
 				enable_attributes=False, extensions=markdown_extensions)
 		elif i == 2:
 			activities = Activity.query \
 				.filter(Activity.end_time > datetime.datetime.now()) \
 				.order_by(Activity.start_time.asc())
-			content += markdown(render_template('activity/view_simple.htm', activities=activities.paginate(1, 12, False)))
+			content += markdown(render_template('activity/view_simple.htm',
+				activities=activities.paginate(1, 12, False)))
 		else:
+			content += '<h1>{0}</h1>'.format(data[i].title)
 			content += markdown(data[i].content, extensions=markdown_extensions)
 
 		content += '</div></div>'
