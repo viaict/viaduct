771363517b37586e53bb15bf5e643817bbbf60ea
Tijmen <tijmen@tijmen-laptop.(none)>
2013-05-26 03:41:09 +0200

    try except om fouten in de parser op te vangen

--- viaduct/blueprints/pimpy/api.py
+++ viaduct/blueprints/pimpy/api.py
@@ -97,7 +97,10 @@ class PimpyAPI:
 			actions = regex.findall(line)
 			print "actions",actions
 			for action in actions:
-				users, title = action.split(":")
+				try:
+					users, title = action.split(":")
+				except:
+					print "could not split the line on ':'.\nSkipping hit."
 				print users
 				print title
 				succes, message = PimpyAPI.commit_task_to_db(title, "", "", group_id,
