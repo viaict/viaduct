73770c651f00bd035f07aa09a69eef47732c6eac
Braamling <bram.vandenakker@student.uva.nl>
2013-07-11 00:54:01 +0200

    test

--- create_db.py
+++ create_db.py
@@ -42,10 +42,10 @@ user = User()
 db.session.add(user)
 db.session.commit()
 
-group.add_user(user)
+# group.add_user(user)
 
-db.session.add(group)
-db.session.commit()
+# db.session.add(group)
+# db.session.commit()
 
 # Add the administrator.
 user = User('administrator@svia.nl', bcrypt.hashpw('ictIsAwesome',
