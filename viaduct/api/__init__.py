86d69718ee3ed777b420a25f35e5f6da613f0d8d
Bas van den Heuvel <bas.vandenheuvel@student.uva.nl>
2013-07-10 16:45:39 +0200

    Removed previously made file models and api.

--- viaduct/api/__init__.py
+++ viaduct/api/__init__.py
@@ -3,14 +3,12 @@ from viaduct import application
 from course import CourseAPI
 from degree import DegreeAPI
 from education import EducationAPI
-from file import FileAPI
 from navigation import NavigationAPI
 from pimpy import PimpyAPI
 
 CourseAPI.register()
 DegreeAPI.register()
 EducationAPI.register()
-FileAPI.register()
 
 application.jinja_env.globals.update(NavigationAPI=NavigationAPI)
 application.jinja_env.globals.update(PimpyAPI=PimpyAPI)
--- viaduct/models/__init__.py
+++ viaduct/models/__init__.py
@@ -4,10 +4,8 @@ from activity import Activity
 from course import Course
 from degree import Degree
 from education import Education
-from file import File
 from group import Group, user_group
 from navigation import NavigationEntry
 from permission import GroupPermission, Permission, UserPermission
 from pimpy import Minute, Task
 from user import User
-
