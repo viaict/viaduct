8775617c8786de89516afe652d92e27745521bf2
Bas van den Heuvel <bas.vandenheuvel@student.uva.nl>
2013-07-10 23:26:51 +0200

    Insert url.

--- viaduct/static/js/search-files.js
+++ viaduct/static/js/search-files.js
@@ -1,5 +1,12 @@
 $(function() {
 	var $results = $('#file-search-results');
+	var $page_content = $('#content');
+
+	var add_url = function() {
+		var url = $(this).attr('href');
+		$page_content.insertAtCaret(url);
+		return false;
+	}
 
 	$('#file-search-query').on('input', function() {
 		var query = $(this).val();
@@ -9,8 +16,15 @@ $(function() {
 
 					$results.empty();
 					for (var i = 0; i < filenames.length; i ++) {
+						var filename = filenames[i];
+
+						var $a = $('<a></a>');
+						$a.attr('href', sprintf('/static/files/%s', filename));
+						$a.text(filenames[i]);
+						$a.on('click', add_url);
+
 						var $li = $('<li></li>');
-						$li.text(filenames[i]);
+						$li.append($a);
 						$results.append($li);
 					}
 				});
