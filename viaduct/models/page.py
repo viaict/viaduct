50e58213690f4c3aace32527b8a7f32b082bc7c4
Braamling <bram.vandenakker@student.uva.nl>
2013-07-11 02:14:34 +0200

    permission bugfix

--- viaduct/models/page.py
+++ viaduct/models/page.py
@@ -95,7 +95,7 @@ class PagePermission(db.Model):
 	def get_user_rights(user, page_id):
 		rights = 0
 
-		if not user.is_active():
+		if user and not user.is_active():
 			groups = [Group.query.filter(Group.name=='all').first()]
 		else:
 			groups = user.groups.all()
