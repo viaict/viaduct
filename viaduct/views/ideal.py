93af69678db7faedc18a671654fb61022e088c65
Tijmen Zwaan <tijmen.zwaan@gmail.com>
2013-12-22 20:26:44 +0100

    first ideal commit

---
+++ viaduct/views/ideal.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, abort, redirect, url_for
+from flask.ext.login import current_user
+
+from viaduct import application, db
+
+blueprint = Blueprint('ideal', __name__, url_prefix='/ideal')
+
+@blueprint.route('')
+def ideal_overview():
+	return abort(404)
