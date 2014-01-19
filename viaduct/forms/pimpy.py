b94691a7b3c05142ec9be04ec048d596d5e8f3a8
Tijmen Zwaan <tijmen.zwaan@gmail.com>
2014-01-19 17:11:16 +0100

    pimpy is now pep8 ready

--- viaduct/forms/pimpy.py
+++ viaduct/forms/pimpy.py
@@ -49,11 +49,18 @@ class EditTaskForm(Form):
     deadline = DateTimeField('Deadline',
                              format=DATE_FORMAT, default=datetime.date.today())
     # timestamp
-    line = IntegerField('Line', default=-1, description='Fill in -1 if this is unknown or impossible.')
-    minute_id = IntegerField('Minute ID', default=-1, description='Fill in -1 if this is unknown or impossible.')
+    line = IntegerField('Line', default=-1,
+                        description='Fill in -1 if this is '
+                                    'unknown or impossible.')
+    minute_id = IntegerField('Minute ID', default=-1,
+                             description='Fill in -1 if this is unknown or '
+                                         'impossible.')
     group = SelectField('Group')
-    users = TextAreaField('Users', description='Type comma separated names for whom this task is, in a similar manner as you are familiar with whilst taking minutes.', validators=[Required()])
-    #status = SelectField('Status', choices=map(lambda x, y: (x, y), range(0, len(Task.status_meanings)), Task.status_meanings), validators=[])
+    users = TextAreaField('Users',
+                          description='Type comma separated names for whom '
+                                      'this task is, in a similar manner as '
+                                      'you are familiar with whilst taking '
+                                      'minutes.', validators=[Required()])
 
     def load_groups(self, groups):
         self.group.choices = map(lambda x: (x.id, x.name), groups)
@@ -61,13 +68,12 @@ class EditTaskForm(Form):
 
 class AddMinuteForm(Form):
     # TODO: should try and resize stuff, especially the content field
-    content = TextAreaField('Content', [validators.required()]) #, validators.length(max=1200)])
+    content = TextAreaField('Content', [validators.required()])
     group = SelectField('Group', [validators.required()])
     # FIXME: datetime is now printed badly in the actual form!!! :( :(
-    date = DateTimeField('Deadline', format=DATE_FORMAT, default=datetime.date.today())
+    date = DateTimeField('Deadline', format=DATE_FORMAT,
+                         default=datetime.date.today())
     parse_tasks = BooleanField('Parse', default=True)
 
     def load_groups(self, groups):
-        #self.group.choices = map(lambda x: ("%s%d" % (x.name, x.id), x.name), groups)
         self.group.choices = map(lambda x: (x.id, x.name), groups)
-
