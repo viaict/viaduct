8fdb6fdd4e3e28dc74341e19489554453bae1617
Tijmen Zwaan <tijmen.zwaan@gmail.com>
2014-01-19 18:39:57 +0100

    mainblock is back on all pages. Expander works on custom_expander class on frontpage

--- viaduct/helpers/__init__.py
+++ viaduct/helpers/__init__.py
@@ -1,5 +1,5 @@
 from flask import flash, request, Markup, url_for, render_template, redirect, \
-	session
+    session
 from flask.ext.login import current_user
 
 
@@ -14,146 +14,146 @@ from viaduct.models.activity import Activity
 import datetime
 
 markdown_extensions = [
-	'toc'
+    'toc'
 ]
 
 
 @login_manager.unauthorized_handler
 def unauthorized():
-	# Save the path the user was rejected from.
-	session['denied_from'] = request.path
+    # Save the path the user was rejected from.
+    session['denied_from'] = request.path
 
-	flash('You must be logged in to view this page.', 'error')
-	return redirect(url_for('user.sign_in'))
+    flash('You must be logged in to view this page.', 'error')
+    return redirect(url_for('user.sign_in'))
 
 
 @application.errorhandler(403)
 def permission_denied(e):
-	""" When permission denied and not logged in you will be redirected. """
-	content = "403, The police has been notified!"
-	image = '/static/img/403.jpg'
+    """ When permission denied and not logged in you will be redirected. """
+    content = "403, The police has been notified!"
+    image = '/static/img/403.jpg'
 
-	# Save the path you were rejected from.
-	session['denied_from'] = request.path
+    # Save the path you were rejected from.
+    session['denied_from'] = request.path
 
-	if not current_user or current_user.is_anonymous():
-		flash('Je hebt geen rechten om deze pagina te bekijken.')
-		return redirect(url_for('user.sign_in'))
+    if not current_user or current_user.is_anonymous():
+        flash('Je hebt geen rechten om deze pagina te bekijken.')
+        return redirect(url_for('user.sign_in'))
 
-	return render_template('page/403.htm', content=content, image=image)
+    return render_template('page/403.htm', content=content, image=image)
 
 
 @application.errorhandler(500)
 def internal_server_error(e):
-	return render_template('page/500.htm')
+    return render_template('page/500.htm')
 
 
 @application.errorhandler(404)
 def page_not_found(e):
-	return render_template('page/404.htm')
+    return render_template('page/404.htm')
 
 
 def flash_form_errors(form):
-	for field, errors in form.errors.items():
-		for error in errors:
-			flash(error, 'error')
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(error, 'error')
 
 
 @application.template_filter('markdown')
 def markdown_filter(data, filter_html=True):
-	if filter_html:
-		safe_mode = False
-	else:
-		safe_mode = 'escape'
+    if filter_html:
+        safe_mode = False
+    else:
+        safe_mode = 'escape'
 
-	return Markup(markdown(data, safe_mode=safe_mode, enable_attributes=False,
-						extensions=markdown_extensions))
+    return Markup(markdown(data, safe_mode=safe_mode, enable_attributes=False,
+                        extensions=markdown_extensions))
 
 
 @application.template_filter('safe_markdown')
 def safe_markdown_filter(data):
-	return Markup(markdown(data, extensions=markdown_extensions))
+    return Markup(markdown(data, extensions=markdown_extensions))
 
 
 @application.template_filter('pages')
 def pages_filter(data):
 
-	content = '<div class="container">'
+    content = '<div class="container">'
 
-	for i in range(len(data)):
-		if i % 2 == 0:
-			content += '<div class="row">'
+    for i in range(len(data)):
+        if i % 2 == 0:
+            content += '<div class="row">'
 
-		if i == len(data) - 1 and i % 2 == 0:
-			content += '<div class="span10">'
-		else:
-			content += '<div class="span6">'
+        if i == len(data) - 1 and i % 2 == 0:
+            content += '<div class="span10">'
+        else:
+            content += '<div class="span6">'
 
-		content += '<div class="mainblock'
-		# expander toevoegen als het over de mainpage gaat
-		content += ' expander">' if data[i].is_main_page else '">'
+        content += '<div class="mainblock'
+        # expander toevoegen als het over de mainpage gaat
+        content += ' custom_expander">' if data[i].is_main_page else '">'
 
-		page = data[i].page if data[i].page.id else None
-		if (page and current_user and (UserAPI.can_write(page) or
-									GroupPermissionAPI.can_write('page')))\
-				or (not page and GroupPermissionAPI.can_write('page')):
-			content += '<div class="btn-group">'
-			content += '<a class="btn" href="' +\
-				url_for('page.get_page_history', path=data[i].path) +\
-				'"><i class="icon-time"></i> View History</a>'
-			content += '<a class="btn" href="' +\
-				url_for('page.edit_page', path=data[i].path) +\
-				'"><i class="icon-pencil"></i> Edit Page</a>'
-			content += '</div>'
+        page = data[i].page if data[i].page.id else None
+        if (page and current_user and (UserAPI.can_write(page) or
+                                    GroupPermissionAPI.can_write('page')))\
+                or (not page and GroupPermissionAPI.can_write('page')):
+            content += '<div class="btn-group">'
+            content += '<a class="btn" href="' +\
+                url_for('page.get_page_history', path=data[i].path) +\
+                '"><i class="icon-time"></i> View History</a>'
+            content += '<a class="btn" href="' +\
+                url_for('page.edit_page', path=data[i].path) +\
+                '"><i class="icon-pencil"></i> Edit Page</a>'
+            content += '</div>'
 
-		# if we render stuff for the main page we want to make sure
-		# the individual pages are rendered correctly, this is super
-		# hard coded but, well, what can you do?
-		if data[i].is_main_page:
-			if data[i].path == 'twitter':
-				if data[i].filter_html:
-					safe_mode = False
-				else:
-					safe_mode = 'escape'
+        # if we render stuff for the main page we want to make sure
+        # the individual pages are rendered correctly, this is super
+        # hard coded but, well, what can you do?
+        if data[i].is_main_page:
+            if data[i].path == 'twitter':
+                if data[i].filter_html:
+                    safe_mode = False
+                else:
+                    safe_mode = 'escape'
 
-				content += '<h1>{0}</h1>'.format(data[i].title)
-				content += markdown(data[i].content, enable_attributes=False,
-									safe_mode=safe_mode,
-									extensions=markdown_extensions)
-			elif data[i].path == 'activities':
-				activities = Activity.query \
-					.filter(Activity.end_time > datetime.datetime.now()) \
-					.order_by(Activity.start_time.asc())
-				content += render_template('activity/view_simple.htm',
-										activities=activities
-										.paginate(1, 12, False))
-			elif data[i].path == 'contact'\
-					or data[i].path == 'laatste_bestuursblog':
-				if data[i].filter_html:
-					safe_mode = False
-				else:
-					safe_mode = 'escape'
+                content += '<h1>{0}</h1>'.format(data[i].title)
+                content += markdown(data[i].content, enable_attributes=False,
+                                    safe_mode=safe_mode,
+                                    extensions=markdown_extensions)
+            elif data[i].path == 'activities':
+                activities = Activity.query \
+                    .filter(Activity.end_time > datetime.datetime.now()) \
+                    .order_by(Activity.start_time.asc())
+                content += render_template('activity/view_simple.htm',
+                                        activities=activities
+                                        .paginate(1, 12, False))
+            elif data[i].path == 'contact'\
+                    or data[i].path == 'laatste_bestuursblog':
+                if data[i].filter_html:
+                    safe_mode = False
+                else:
+                    safe_mode = 'escape'
 
-				content += '<h1>{0}</h1>'.format(data[i].title)
-				content += markdown(data[i].content, safe_mode=safe_mode,
-									extensions=markdown_extensions)
-		else:
-			if data[i].filter_html:
-				safe_mode = False
-			else:
-				safe_mode = 'escape'
+                content += '<h1>{0}</h1>'.format(data[i].title)
+                content += markdown(data[i].content, safe_mode=safe_mode,
+                                    extensions=markdown_extensions)
+        else:
+            if data[i].filter_html:
+                safe_mode = False
+            else:
+                safe_mode = 'escape'
 
-			#print data[i].path
-			content += '<h1>{0}</h1>'.format(data[i].title)
-			content += '<h2>Hello</h2>'
-			content += markdown(data[i].content, safe_mode=safe_mode,
-								extensions=markdown_extensions)
+            #print data[i].path
+            content += '<h1>{0}</h1>'.format(data[i].title)
+            content += '<h2>Hello</h2>'
+            content += markdown(data[i].content, safe_mode=safe_mode,
+                                extensions=markdown_extensions)
 
-		content += '</div></div>'
+        content += '</div></div>'
 
-		if i == len(data) - 1 or i % 2 != 0:
-			content += '</div>'
+        if i == len(data) - 1 or i % 2 != 0:
+            content += '</div>'
 
-	content += '</div>'
+    content += '</div>'
 
-	return Markup(content)
+    return Markup(content)
--- viaduct/static/js/custom.js
+++ viaduct/static/js/custom.js
@@ -1,50 +1,50 @@
 $(document).ready(function() {
-	$('.mainblock').each(function() {
-		var desired_height_text = $(this).css('min-height');
-		var desired_height = parseInt(desired_height_text.
-									substr(0, desired_height_text.length - 2));
+    $('.custom_expander').each(function() {
+        var desired_height_text = $(this).css('min-height');
+        var desired_height = parseInt(desired_height_text.
+                                    substr(0, desired_height_text.length - 2));
 
-		var actual_height = Math.floor($(this).height());
+        var actual_height = Math.floor($(this).height());
 
-		if (actual_height > desired_height) {
-			$main_block = $(this);
+        if (actual_height > desired_height) {
+            $main_block = $(this);
 
-			$(this).css({'overflow': 'hidden',
-						'height': sprintf('%dpx', desired_height),
-						'position': 'relative'});
+            $(this).css({'overflow': 'hidden',
+                        'height': sprintf('%dpx', desired_height),
+                        'position': 'relative'});
 
-			var $read_more_div = $('<div></div>');
-			$read_more_div.css({'height': $(this).css('margin-bottom'),
-								'left': sprintf('-%s',
-												$(this).css('margin-left')),
-								'padding': $(this).css('margin'),
-								'padding-left': $(this).css('padding-left'),
-								'width': $(this).css('width')});
-			$read_more_div.addClass('readmore');
+            var $read_more_div = $('<div></div>');
+            $read_more_div.css({'height': $(this).css('margin-bottom'),
+                                'left': sprintf('-%s',
+                                                $(this).css('margin-left')),
+                                'padding': $(this).css('margin'),
+                                'padding-left': $(this).css('padding-left'),
+                                'width': $(this).css('width')});
+            $read_more_div.addClass('readmore');
 
-			var $read_more_link = $('<a>Meer lezen</a>');
-			$read_more_link.attr('href', '#');
-			$read_more_link.on('click', function() {
-				if (Math.floor($main_block.height()) == desired_height) {
-					$main_block.animate({'height': sprintf('%dpx',
-										actual_height + 20),
-										'overflow': 'visible'});
-					$(this).text('Minder lezen');
-				}
-				else {
-					$main_block.animate({'overflow': 'hidden',
-									'height': sprintf('%dpx', desired_height)});
-					$(this).text('Meer lezen');
-				}
+            var $read_more_link = $('<a>Meer lezen</a>');
+            $read_more_link.attr('href', '#');
+            $read_more_link.on('click', function() {
+                if (Math.floor($main_block.height()) == desired_height) {
+                    $main_block.animate({'height': sprintf('%dpx',
+                                        actual_height + 20),
+                                        'overflow': 'visible'});
+                    $(this).text('Minder lezen');
+                }
+                else {
+                    $main_block.animate({'overflow': 'hidden',
+                                    'height': sprintf('%dpx', desired_height)});
+                    $(this).text('Meer lezen');
+                }
 
-				return false;
-			});
+                return false;
+            });
 
-			$read_more_div.append($read_more_link);
+            $read_more_div.append($read_more_link);
 
-			$(this).prepend($read_more_div);
-		}
-	});
+            $(this).prepend($read_more_div);
+        }
+    });
 });
 
 /* PimPy function
@@ -55,40 +55,40 @@ $(document).ready(function() {
 $.fn.editable.defaults.mode = 'inline';
 
 function update_task_status($task_id, $new_status) {
-	$.getJSON('/pimpy/tasks/update_status', {
-		task_id: $task_id,
-		new_status: $new_status
-	}, function(data) {
-		$id = 'pimpy_task'+$task_id;
-		$('#'+$id).attr('class', "btn dropdown-toggle " + data.status);
-	});
-	return false;
+    $.getJSON('/pimpy/tasks/update_status', {
+        task_id: $task_id,
+        new_status: $new_status
+    }, function(data) {
+        $id = 'pimpy_task'+$task_id;
+        $('#'+$id).attr('class', "btn dropdown-toggle " + data.status);
+    });
+    return false;
 }
 
 function edit_task($task_id) {
-	$result = $.ajax({url:'/pimpy/tasks/edit/' + $task_id});
-	console.log($result);
-	return false;
-	$.getJSON('/pimpy/tasks/edit/' + $task_id, {
-	}, function(data) {
-	});
+    $result = $.ajax({url:'/pimpy/tasks/edit/' + $task_id});
+    console.log($result);
+    return false;
+    $.getJSON('/pimpy/tasks/edit/' + $task_id, {
+    }, function(data) {
+    });
 
 }
 
 function update_task($task_id, $task_name, $more_info,
-	$deadline, $group, $users, $status) {
+    $deadline, $group, $users, $status) {
 }
 
 $(document).ready(function() {
-	$('.pimpy_editable').editable();
-	$('.pimpy_editable_toggle').click(function() {
-		$('.pimpy_editable').editable('toggleDisabled');
-	});
-	$('.pimpy_editable_date').editable({
-		format: 'yyyy-mm-dd',
-		viewformat: 'dd/mm/yyyy',
-		datepicker: {
-			weekStart: 1
-		}
-	});
+    $('.pimpy_editable').editable();
+    $('.pimpy_editable_toggle').click(function() {
+        $('.pimpy_editable').editable('toggleDisabled');
+    });
+    $('.pimpy_editable_date').editable({
+        format: 'yyyy-mm-dd',
+        viewformat: 'dd/mm/yyyy',
+        datepicker: {
+            weekStart: 1
+        }
+    });
 });
