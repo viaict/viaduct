254308365d0231743f4d74e455c555278238dac7
Jelte Fennema <github@jeltef.nl>
2014-06-09 07:46:22 +1000

    Update watch.sh, .gitignore and .vim.custom to work with Grunt

--- watch.sh
+++ watch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # source: http://stackoverflow.com/questions/9023164/in-bash-how-can-i-run-multiple-infinitely-running-commands-and-cancel-them-all
 
-#grunt dev
+grunt dev
 compass watch . &
 PIDS[0]=$!
 
-#grunt watch &
-#PIDS[1]=$!
+grunt watch &
+PIDS[1]=$!
 
 python run.py
 PIDS[2]=$!
