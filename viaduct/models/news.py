428c6efe4905caa9dae5a079dd7161b5a9977432
Maarten Inja <maarten.inja@gmail.com>
2014-06-08 21:10:36 +0200

    author to user

--- viaduct/models/news.py
+++ viaduct/models/news.py
@@ -8,8 +8,8 @@ class NewsRevision(IdRevision):
     content = db.Column(db.Text)
     end_time = db.Column(db.Date)
 
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    author = db.relationship('User', backref=db.backref('news_revisions',
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user= db.relationship('User', backref=db.backref('news_revisions',
                                                         lazy='dynamic'))
 
     page_id = db.Column(db.Integer, db.ForeignKey('page.id'))
@@ -17,12 +17,12 @@ class NewsRevision(IdRevision):
                                                       lazy='dynamic'))
 
     def __init__(self, page=None, title='', comment='', instance_id=None,
-                 content='', author_id=None, end_time=None):
+                 content='', user_id=None, end_time=None):
         super(NewsRevision, self).__init__(title, comment, instance_id)
 
         self.page = page
 
-        self.author_id = author_id
+        self.user_id = user_id
         self.content = content
         self.end_time = end_time
 
