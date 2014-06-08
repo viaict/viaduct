c9de86c882159b9cb5718ecc0a7cab6e4778207e
Jelte Fennema <github@jeltef.nl>
2014-06-09 07:37:01 +1000

    Add Grunt rules

--- src/js/single/formbuilder.js
+++ src/js/single/formbuilder.js
@@ -2,7 +2,7 @@
 
 $.fn.formbuilder = function() {
 
-	var info = $('<div class="alert alert-info"><b>Formulier commandos</b> Naam | type ' +
+	var info = $('<div class="alert alert-info"><b>Formulier commandos</b> Naam | type ' + 
 							 '(Waar de type een "textarea", "radio", "checkbox" of "select" is)' +
 							 '<p>Radio, checkbox en select opties maak je aan door de regels met <b>"-"</b> te beginnen. <p>Verder zijn er een paar shortcut "types" ("weekend", "shirt"), om automatisch meerdere velden in één keer aan te maken. <p><b>Bijvoorbeeld:</b><br>Dieet | checkbox<br>- Vlees<br>- Vegetarisch<br>- Veganist' +
 '</div>');
@@ -12,7 +12,7 @@ $.fn.formbuilder = function() {
   var group    = $('<div class="control-group" />');
   var controls = $('<div class="controls" />');
   var label    = $('<label class="control-label" />');
-
+  
   var fields = {
     'text'     : $('<input type="text" name>'),
     'radio'    : $('<div />'),
@@ -31,7 +31,7 @@ $.fn.formbuilder = function() {
   textarea.on('keyup', function() {
     form.html(''); // Reset the control
     var lines = this.value.split("\n");
-
+    
 
     for (var i=0; i < lines.length; i++) {
 
@@ -61,22 +61,22 @@ $.fn.formbuilder = function() {
 				form.find('label').last().after('<small>' + lines[i].substring(1) + '</small>');
         continue;
       }
-
+      
       options = parseLine(lines[i]);
       type    = options.type || "text";
 
 			if (type === "weekend") {
 				lines[i] = '';
-        lines.splice(i, 0,
-					'Dieet | checkbox',
-					'-Vegetarisch',
-					'-Veganistisch',
+        lines.splice(i, 0, 
+					'Dieet | checkbox', 
+					'-Vegetarisch', 
+					'-Veganistisch', 
 
 					'shirt',
 					'Noodnummer*',
-					'&lt; Telefoon nummer in geval van nood',
+					'> Telefoon nummer in geval van nood',
 					'Allergie/medicatie | textarea',
-					'&lt; Waar moeten we rekening mee houden'
+					'> Waar moeten we rekening mee houden'
 				);
 
         textarea.val(lines.join("\n"));
@@ -86,7 +86,7 @@ $.fn.formbuilder = function() {
 
 			if (type === "shirt") {
 				lines[i] = '';
-				lines.splice(i, 0, '',
+				lines.splice(i, 0, '', 
 					'Shirt* maat | select',
 					'-Small',
 					'-Medium',
@@ -112,10 +112,10 @@ $.fn.formbuilder = function() {
               .attr('name', options.name)
               .attr('id',   options.id)
          ));
-
+      
       if (type === 'select' || type === 'radio' || type === 'checkbox') {
         fields[type].html(''); // Reset the list
-
+        
         while (++i < lines.length && lines[i] && (lines[i].charAt(0) == '*' || lines[i].charAt(0) == '-')) {
           value = strip_(lines[i]);
 
@@ -124,21 +124,21 @@ $.fn.formbuilder = function() {
           else
             fields[type].append(
               '<label class="checkbox">' +
-                '<input type="' + type + '" name="' + options.name + '[]" value="' + value + '">' +
-                value +
+                '<input type="' + type + '" name="' + options.name + '[]" value="' + value + '">' + 
+                value + 
               '</label>'
             );
         }
-
+        
         i--; // Go back 1 step to parse the next element without #hashtag
       }
-
+      
       group.clone().appendTo(form);
     }
-
+    
     result.val('<div id="custom_form_data">' + form.html() + '</div>');
   });
-
+  
 	function strip(str) {
 		return str.replace(/[\+\*\-]/g, '');
 	}
@@ -155,18 +155,18 @@ $.fn.formbuilder = function() {
 
     if (label == "") // Skip empty lines
       return false;
-
+          
     var options = {
       'label'    : label,
       'name'     : name,
-      'required' : line[0].indexOf('*') > 0 ? "true" : "false"
+      'required' : line[0].indexOf('*') > 0 ? "true" : "false" 
     };
-
+    
     if (line[1])
       options['type'] = strip_(line[1]);
 		else if (options.name === 'weekend' || options.name === 'shirt')
 			options['type'] = options.name;
 
     return options;
-  }
+  }  
 };
