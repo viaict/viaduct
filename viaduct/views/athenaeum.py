99df4a14de361acf72002a3436df68f200985182
Bram van den Akker <bram.vandenakker@cameramanager.com>
2014-08-17 10:49:10 +0200

    athenaeum with https

--- viaduct/views/athenaeum.py
+++ viaduct/views/athenaeum.py
@@ -7,6 +7,6 @@ blueprint = Blueprint('athenaeum', __name__, url_prefix='/athenaeum')
 @blueprint.route('/', methods=['GET'])
 def embed():
     """Embed the athenaeum website."""
-    url = 'http://athenaeum-sv.mijnboekhandelaar.com/'
+    url = 'https://athenaeum-sv.mijnboekhandelaar.com/'
 
     return render_template('athenaeum/embed.htm', url=url)
