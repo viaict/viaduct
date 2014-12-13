a5a58fa1a6adc8231ccaceb172a1724f163af800
Jelte Fennema <github-tech@jeltef.nl>
2014-12-13 16:12:22 +0100

    VIA-61 #resolve Hopefully all are gone now

--- src/js/single/elections/nominate.js
+++ src/js/single/elections/nominate.js
@@ -37,7 +37,7 @@ $(function() {
             var error = $.parseJSON(jqHXR.responseText).error;
 
             clearflash();
-            flash(error, 'error');
+            flash(error, 'danger');
 
             $btn.prop('disabled', false);
         });
@@ -62,7 +62,7 @@ $(function() {
             var error = $.parseJSON(jqHXR.responseText).error;
 
             clearflash();
-            flash(error, 'error');
+            flash(error, 'danger');
 
             $removes.removeClass('disabled');
         });
