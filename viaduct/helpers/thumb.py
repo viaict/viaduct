574324399cf405ec215690e93d0a3de01948e1e3
Tijmen Zwaan <tijmen.zwaan@gmail.com>
2014-12-23 01:20:05 +0100

    Thumbnails can now be of multiple sizes

--- viaduct/helpers/thumb.py
+++ viaduct/helpers/thumb.py
@@ -8,9 +8,16 @@ def thumb(url, size=(100, 100)):
 
     im_dir, im_name = os.path.split(im_path)
 
-    thmb_dir = os.path.join(im_dir, '.thumb')
-    thmb_path = os.path.join(thmb_dir, im_name)
+    im_title, im_ext = im_name.rsplit('.', 1)
 
+    thmb_name = im_title + '-' + str(size[0]) + 'x' \
+        + str(size[1]) + '.' + im_ext
+
+    thmb_dir = os.path.join(im_dir, '.thumb')
+    thmb_path = os.path.join(thmb_dir, thmb_name)
+
+    if not os.path.exists(im_path):
+        return url
     if not os.path.exists(thmb_dir):
         # TODO: Fix rights.
         os.mkdir(thmb_dir)
@@ -20,6 +27,6 @@ def thumb(url, size=(100, 100)):
         im.save(thmb_path)
 
     slash = url.rindex('/')
-    thmb_url = url[:slash] + '/.thumb' + url[slash:]
+    thmb_url = url[:slash] + '/.thumb/' + thmb_name
 
     return thmb_url
