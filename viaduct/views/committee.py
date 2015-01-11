df8f0352615dacda7687128be68f8e16fbce04ca
Bas van den Heuvel <bas.vandenheuvel@student.uva.nl>
2015-01-11 13:08:31 +0100

    Add a navigation entry for every new committee page created

--- viaduct/views/committee.py
+++ viaduct/views/committee.py
@@ -3,7 +3,8 @@ from flask import Blueprint, abort, request, flash, redirect, url_for, \
 from flask.ext.login import current_user
 
 from viaduct import db
-from viaduct.models import CommitteeRevision, Page, Group, User
+from viaduct.models import CommitteeRevision, Page, Group, User, \
+    NavigationEntry
 from viaduct.api import GroupPermissionAPI
 from viaduct.forms import CommitteeForm
 from viaduct.helpers import flash_form_errors
@@ -52,8 +53,50 @@ def edit_committee(committee=''):
         selected_group.users.order_by(User.first_name, User.last_name).all()]
 
     if form.validate_on_submit():
+        committee_title = data['title'].strip()
+
         if not page:
+            root_entry = NavigationEntry.query\
+                .filter(NavigationEntry.url == url_for('committee.list'))\
+                .first()
+
+            # Check whether the root navigation entry exists.
+            if not root_entry:
+                last_root_entry = NavigationEntry.query\
+                    .filter(NavigationEntry.parent_id == None)\
+                    .order_by(NavigationEntry.position.desc()).first()  # noqa
+
+                root_entry_position = 1
+                if last_root_entry:
+                    root_entry_position = last_root_entry.position + 1
+
+                root_entry = NavigationEntry(
+                    None, 'Commissies', url_for('committee.list'), False,
+                    False, root_entry_position)
+
+                db.session.add(root_entry)
+                db.session.commit()
+
             page = Page(path, 'committee')
+
+            # Create a navigation entry for the new committee.
+            last_navigation_entry = NavigationEntry.query\
+                .filter(NavigationEntry.parent_id == root_entry.id)\
+                .first()
+
+            entry_position = 1
+            if last_navigation_entry:
+                entry_position = last_navigation_entry.position + 1
+
+            navigation_entry = NavigationEntry(
+                root_entry, committee_title, '/' + path, False, False,
+                entry_position)
+
+            db.session.add(navigation_entry)
+            db.session.commit()
+
+            # Assign the navigation entry to the new page (committee).
+            page.navigation_entry_id = navigation_entry.id
 
             db.session.add(page)
             db.session.commit()
@@ -62,7 +105,7 @@ def edit_committee(committee=''):
         coordinator_id = int(data['coordinator_id'])
 
         new_revision = CommitteeRevision(
-            page, data['title'].strip(), data['comment'].strip(),
+            page, committee_title, data['comment'].strip(),
             current_user.id, data['description'].strip(), group_id,
             coordinator_id, 'interim' in data)
 
