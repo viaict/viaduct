eac13a3a21f8e1b5dd9f014e4ef0d374a95fddf3
Maico Timmerman <maico.timmerman@gmail.com>
2015-02-14 16:29:35 +0100

    Added a dynamic parameter for type.

--- src/js/global/utils.js
+++ src/js/global/utils.js
@@ -23,27 +23,35 @@ utils.datatables.enable_select = function ($selector) {
     } );
 };
 
-utils.datatables.delete_by_url = function($selector, table, url) {
-    $selector.click( function () {
-        var selected_ids = [];
-        var selected_users = "";
-        var selected_rows = table.rows('.info');
-        for (i = 0; i < selected_rows.data().length; i++) {
-            selected_ids.push(selected_rows.data()[i][0]);
-            selected_users += "- " + selected_rows.data()[i][1] + "\n";
-        }
-        var json_data = { "selected_ids" : selected_ids };
-        if (confirm('Do you surely want to delete all these?\n' + selected_users )) {
+utils.datatables.get_ids = function($selector, table) {
+    var selected_ids = [];
+    var selected_users = "";
+    var selected_rows = table.rows('.info');
+    for (i = 0; i < selected_rows.data().length; i++) {
+        selected_ids.push(selected_rows.data()[i][0]);
+        selected_users += "- " + selected_rows.data()[i][1] + "\n";
+    }
+    return { "selected_ids" : selected_ids };
+}
+
+utils.datatables.action_by_url = function(
+    $selector, table, url, action, refer) {
+    $selector.click(function () {
+        var json_data = utils.datatables.get_ids($selector, table);
+        if (confirm('Are you sure you want to ' + action + '?\n')) {
             $.ajax({
-                type: "DELETE",
+                type: action,
                 url: url,
                 contentType: 'application/json',
                 success: function (data) {
-                    table.ajax.reload();
+                    if (action === "put") {
+                        window.open(refer, "_top");
+                    } else {
+                        table.ajax.reload();
+                    }
                 },
                 data: JSON.stringify(json_data, null, '\t'),
             });
-
         } else {
             return;
         }
