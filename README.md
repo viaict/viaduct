1375b7de35a4d33666794625d7314f7f48a39fcf
Maico Timmerman <maico.timmerman@gmail.com>
2015-02-15 20:45:48 +0100

    Hotfix for python 2->3 conversion in group permissions

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Version.
-2.0.0.1
+2.0.0.2
 
 #Viaduct (Opensourced, yeah)
 ## Tutorial
--- viaduct/__init__.py
+++ viaduct/__init__.py
@@ -11,7 +11,7 @@ import datetime
 import json
 
 
-version = 'v2.0.0.1'
+version = 'v2.0.0.2'
 
 
 def static_url(url):
