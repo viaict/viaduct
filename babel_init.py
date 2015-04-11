6b0129d7426ae4cf8d1f46656d5be81a0073d74d
Maico Timmerman <maico.timmerman@gmail.com>
2015-04-11 17:34:01 +0200

    Updated the babel libraries, always generate both locales

--- babel_init.py
+++ babel_init.py
@@ -1,12 +1,8 @@
 #!venv/bin/python
 import os
-import sys
 pybabel = 'venv/bin/pybabel'
-if len(sys.argv) != 2:
-    print("usage: babel_init <language-code>")
-    sys.exit(1)
 os.system(pybabel + ' extract -F babel.cfg -k lazy_gettext -o messages.pot \
           viaduct')
-os.system(pybabel + ' init -i messages.pot -d viaduct/translations -l '
-          + sys.argv[1])
-os.unlink('messages.pot')
+os.system(pybabel + ' init -i messages.pot -d viaduct/translations -l en')
+os.system(pybabel + ' init -i messages.pot -d viaduct/translations -l nl')
+os.remove('messages.pot')
