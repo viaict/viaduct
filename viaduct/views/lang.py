9e6886c95dd3082797b3be82d90bd76eb318cea6
Maico Timmerman <maico.timmerman@gmail.com>
2015-07-14 22:47:33 +0200

    Added a language switcher support
    The language of the user is switched to the selected view, restricted by
    config. If the current user is anonymous (not logged in) then a client
    side cookie is set with the language in it.
    
    On request the locale from the user is used, if the current user is
    anoymous then the language from the set cookie is taken. If no cookie is
    found, the best matching language from the config is taken based on
    request headers.
    
    Locale selection function is now also available in templates to be able
    to show the current used language in the base template.

---
+++ viaduct/views/lang.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from viaduct import db
+from flask import Blueprint, redirect, make_response, request, url_for, flash
+from flask.ext.babel import refresh
+from flask.ext.login import current_user
+from flask.ext.babel import _
+from config import LANGUAGES
+
+blueprint = Blueprint('lang', __name__, url_prefix='/lang')
+
+
+def redirect_url(default='home.home'):
+    return request.args.get('next') or request.referrer or url_for(default)
+
+
+@blueprint.route('/<path:lang>', methods=['GET'])
+def set_lang(lang=None):
+    if lang not in LANGUAGES.keys():
+        flash(_('Language unsupported on this site') + ': ' + lang, 'warning')
+        return redirect(url_for('home.home'))
+
+    if current_user.is_anonymous():
+        rv = make_response(redirect(redirect_url()))
+        rv.set_cookie('lang', lang)
+        refresh()
+        return rv
+    else:
+        current_user.locale = lang
+        db.session.add(current_user)
+        db.session.commit()
+        refresh()
+        flash(_('Language changed to') + ': ' + LANGUAGES[lang], 'success')
+        return redirect(redirect_url())
