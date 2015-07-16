8545dd408a20baa92c2aa1bf6d2cc0e85dbd87f7
Maico Timmerman <maico.timmerman@gmail.com>
2015-07-16 10:23:27 +0200

    Updated database model form dual language implementation
    Column for name and description are duplicated with a prefix for Dutch
    and English language.
    
    All strings are now babel lazy evaluated strings, causing the to be
    looked up on request in the .po files.
    
    Removed the separation of timedelta calculation and formatation of it.
    Reducing the number of nested function calls for one line of code.
    
    On loading of the Activity model the name and description properties are
    set based on cookie, headers or saved locale. Those properties were used
    everywhere in viaduct.
    
    If set language is unavailable for the current activity, and suffix is
    added to notify the user of content in a different language.

--- viaduct/models/activity.py
+++ viaduct/models/activity.py
@@ -1,7 +1,10 @@
 from viaduct import db
-import datetime
+from viaduct import get_locale
+from datetime import datetime, timedelta
 from viaduct.models import BaseEntity
-from babel.dates import format_timedelta
+from babel.dates import format_timedelta, format_datetime
+from sqlalchemy import event
+from flask.ext.babel import lazy_gettext as _  # gettext
 
 
 # Model to support Facebook/Google API integration
@@ -11,8 +14,10 @@ class Activity(db.Model, BaseEntity):
     prints = ('id', 'name', 'start_time', 'end_time')
 
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    name = db.Column(db.String(256))
-    description = db.Column(db.String(2048))
+    en_name = db.Column(db.String(256))
+    en_description = db.Column(db.String(2048))
+    nl_name = db.Column(db.String(256))
+    nl_description = db.Column(db.String(2048))
     start_time = db.Column(db.DateTime)
     end_time = db.Column(db.DateTime)
     location = db.Column(db.String(64))
@@ -20,26 +25,35 @@ class Activity(db.Model, BaseEntity):
     price = db.Column(db.String(16))
     picture = db.Column(db.String(255))
     venue = db.Column(db.Integer)  # venue ID
-    updated_time = db.Column(db.DateTime, default=datetime.datetime.now())
+    updated_time = db.Column(db.DateTime, default=datetime.now())
     form_id = db.Column(db.Integer, db.ForeignKey('custom_form.id'))
 
     google_event_id = db.Column(db.String(64))
-    owner = db.relationship('User', backref=db.backref('activities',
-                                                       lazy='dynamic'))
+    owner = db.relationship('User',
+                            backref=db.backref('activities', lazy='dynamic'))
 
-    def __init__(self, owner_id=None, name="", description="", start_time=None,
-                 end_time=None, location="Sciencepark, Amsterdam",
-                 privacy="public", price="gratis", picture=None, venue=1,
-                 form_id=None):
+    def __init__(self, owner_id=None, en_name="", en_description="",
+                 nl_name="", nl_description="", start_time=None,
+                 end_time=None,
+                 location="Studievereniging VIA, Science Park 904, "
+                 "1098 XH Amsterdam", privacy=_("Public"),
+                 price=_("Free"), picture=None, venue=1, form_id=None):
+
         if not start_time:
-            today = datetime.datetime.now()
-            start_time = datetime.datetime(today.year, today.month, today.day,
-                                           17)
-            end_time = start_time + datetime.timedelta(hours=5)
+            today = datetime.now()
+            start_time = datetime(today.year, today.month, today.day, 17)
+            end_time = start_time + timedelta(hours=5)
 
         self.owner_id = owner_id
-        self.name = name
-        self.description = description
+
+        self.name = None
+        self.nl_name = nl_name
+        self.en_name = en_name
+
+        self.description = None
+        self.nl_description = nl_description
+        self.en_description = en_description
+
         self.start_time = start_time
         self.end_time = end_time
         self.location = location
@@ -50,7 +64,11 @@ class Activity(db.Model, BaseEntity):
         self.form_id = form_id
 
     def get_time(self):
-        today = datetime.datetime.now()
+        """
+        Get a proper representation of all datetime date, based on the time the
+        event is gonna take/when it is going to start/when it's gonna end
+        """
+        today = datetime.now()
         if self.start_time.month == self.end_time.month and \
                 self.start_time.day == self.end_time.day:
             if self.start_time.year == today.year:
@@ -68,10 +86,10 @@ class Activity(db.Model, BaseEntity):
                     self.end_time.strftime("%a. %d %b %Y (%H:%M)")
 
     def is_in_future(self):
-        return datetime.datetime.now() < self.start_time
+        return datetime.now() < self.start_time
 
     def is_in_past(self):
-        return datetime.datetime.now() >= self.end_time
+        return datetime.now() >= self.end_time
 
     def get_short_description(self, characters):
         if (len(self.description) > characters):
@@ -83,34 +101,66 @@ class Activity(db.Model, BaseEntity):
         return self.description
 
     def get_timedelta_to_start(self):
-        return datetime.datetime.now() - self.start_time
-
-    def get_timedelta_to_start_formatted(self, locale="nl_NL"):
         """
-        Returns over 1 dag
+        Returns locale based description of time left to start.
         """
-        return format_timedelta(self.get_timedelta_to_start(), locale=locale)
+        return format_timedelta(datetime.now() - self.start_time,
+                                locale=get_locale())
 
     def get_timedelta_from_end(self):
-        return datetime.datetime.now() - self.end_time
-
-    def get_timedelta_from_end_formatted(self, locale='nl_NL'):
-        return format_timedelta(self.get_timedelta_from_end(), locale=locale)
+        """
+        Returns locale based description of time in the past.
+        """
+        return format_timedelta(datetime.now() - self.end_time,
+                                locale=get_locale())
 
     def get_timedelta_to_end(self):
-        return self.end_time - datetime.datetime.now()
-
-    def get_timedelta_to_end_formatted(self, locale='nl_NL'):
-        return format_timedelta(self.get_timedelta_to_end(), locale=locale)
+        """
+        Returns locale based description of time until the end of the activity.
+        """
+        return format_timedelta(self.end_time - datetime.now(),
+                                locale=get_locale())
 
     def format_form_time(self, time):
         return time.strftime("%Y-%m-%d %H:%M")
 
     def till_now(self):
+        """
+        Returns a locale based description of datetimedelta till now
+        """
         if self.is_in_future():
-            return 'over %s' % (self.get_timedelta_to_start_formatted())
+            return _('in') + ' %s' % (self.get_timedelta_to_start())
 
         if self.is_in_past():
-            return '%s geleden' % (self.get_timedelta_from_end_formatted())
+            return '%s ' % (self.get_timedelta_from_end()) + _('ago')
 
-        return 'nog %s' % (self.get_timedelta_to_end_formatted())
+        return _('still') + ' %s' % (self.get_timedelta_to_end())
+
+
+@event.listens_for(Activity, 'load')
+def set_activity_locale(activity, context):
+    """
+    This function is called after an Activity model is filled with data from
+    the database, but before is used in all other code.
+
+    Use the locale of the current user/client to determine which language to
+    display on the whole website.
+    """
+    locale = get_locale()
+    nl_available = activity.nl_name and activity.nl_description
+    en_available = activity.en_name and activity.en_description
+    if locale == 'nl' and nl_available:
+        activity.name = activity.nl_name
+        activity.description = activity.nl_description
+    elif locale == 'en' and en_available:
+        activity.name = activity.en_name
+        activity.description = activity.en_description
+    elif nl_available:
+        activity.name = activity.nl_name + " (" + _('Dutch') + ")"
+        activity.description = activity.nl_description
+    elif en_available:
+        activity.name = activity.en_name + " (" + _('English') + ")"
+        activity.description = activity.en_description
+    else:
+        activity.name = 'N/A'
+        activity.description = 'N/A'
