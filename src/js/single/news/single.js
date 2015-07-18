97bbcb44c8b043e62f74049e348dfdb0cd87e855
Maico Timmerman <maico.timmerman@gmail.com>
2015-07-18 13:20:32 +0200

    Added loading state to the submit of the news item

--- src/js/single/news/single.js
+++ src/js/single/news/single.js
@@ -1,12 +1,5 @@
-$(document).ready(function() {
-    var $delete_form = $('form#delete');
-    var $delete_btn = $('button#btn-delete');
-
-    $delete_form.hide();
-
-    $delete_btn.click(function() {
-        if (confirm('Weet u zeker dit nieuwsartikel te willen verwijderen?')) {
-            $delete_form.submit();
-        }
+$(function() {
+    $('#create_news').on('submit', function(event){
+        $('button', this).button('loading');
     });
 });
