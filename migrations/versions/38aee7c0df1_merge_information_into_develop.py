048714a9c6145ee6cf0d4431f541b49861083bcc
Jaquim Cadogan <jaquimcadogan@hotmail.com>
2015-07-18 14:04:44 +0200

    Added option to receive company information

---
+++ migrations/versions/38aee7c0df1_merge_information_into_develop.py
@@ -0,0 +1,22 @@
+"""Merge information into develop
+
+Revision ID: 38aee7c0df1
+Revises: ('12b99e99f10', '48898228514')
+Create Date: 2015-07-18 14:03:38.494512
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '38aee7c0df1'
+down_revision = ('12b99e99f10', '48898228514')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
