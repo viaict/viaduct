e7836014b3707caf436a89485d852dbc4441c48b
Bram van den Akker <contact@bramvandenakker.nl>
2015-07-18 12:41:22 +0200

    fixed uppercase filenames

--- viaduct/views/examination.py
+++ viaduct/views/examination.py
@@ -30,7 +30,7 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 def allowed_file(filename):
     return '.' in filename and \
-        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 def file_exists(filename):
