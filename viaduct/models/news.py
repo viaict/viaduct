cbb33b7801ceb9ed4f3b2eb41c5b3d9c5ab6c92f
Maico Timmerman <maico.timmerman@gmail.com>
2015-07-19 14:15:21 +0200

    Updated news for dual language content

--- viaduct/models/news.py
+++ viaduct/models/news.py
@@ -1,15 +1,20 @@
 from datetime import date
+from sqlalchemy import event
+
 from flask import url_for
-from viaduct import db
-from viaduct.models import BaseEntity
 from flask.ext.babel import lazy_gettext as _
+
+from viaduct import db, get_locale
+from viaduct.models import BaseEntity
 
 
 class News(db.Model, BaseEntity):
     __tablename__ = 'news'
 
-    title = db.Column(db.Text)
-    content = db.Column(db.Text)
+    nl_title = db.Column(db.Text)
+    en_title = db.Column(db.Text)
+    nl_content = db.Column(db.Text)
+    en_content = db.Column(db.Text)
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = db.relationship('User', backref=db.backref('news', lazy='dynamic'))
@@ -17,11 +22,16 @@ class News(db.Model, BaseEntity):
     publish_date = db.Column(db.Date)
     archive_date = db.Column(db.Date)
 
-    def __init__(self, title='', content='', user_id=None,
-                 archive_date=None, publish_date=date.today()):
+    def __init__(self, nl_title='', nl_content='', en_title='', en_content='',
+                 user_id=None, archive_date=None, publish_date=date.today()):
 
-        self.title = title
-        self.content = content
+        self.title = None
+        self.content = None
+        self.nl_title = nl_title
+        self.nl_content = nl_content
+        self.en_title = en_title
+        self.en_content = en_content
+
         self.user_id = user_id
         self.archive_date = archive_date
         self.publish_date = publish_date
@@ -39,3 +49,34 @@ class News(db.Model, BaseEntity):
                 '">(' + _('Read more') + '...)</a></small>'
 
         return self.content
+
+
+@event.listens_for(News, 'load')
+def set_activity_locale(activity, context):
+    """
+    This function is called after an Activity model is filled with data from
+    the database, but before is used in all other code.
+
+    Use the locale of the current user/client to determine which language to
+    display on the whole website. If the users locale is unavailable, select
+    the alternative language, suffixing the title of the activity with the
+    displayed language.
+    """
+    locale = get_locale()
+    nl_available = activity.nl_title and activity.nl_content
+    en_available = activity.en_title and activity.en_content
+    if locale == 'nl' and nl_available:
+        activity.title = activity.nl_title
+        activity.content = activity.nl_content
+    elif locale == 'en' and en_available:
+        activity.title = activity.en_title
+        activity.content = activity.en_content
+    elif nl_available:
+        activity.title = activity.nl_title + " (" + _('Dutch') + ")"
+        activity.content = activity.nl_content
+    elif en_available:
+        activity.title = activity.en_title + " (" + _('English') + ")"
+        activity.content = activity.en_content
+    else:
+        activity.title = 'N/A'
+        activity.content = 'N/A'
