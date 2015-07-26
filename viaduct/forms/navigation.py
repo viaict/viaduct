484db84a9823b4aaf8ac6eef650da168883e1819
Maico Timmerman <maico.timmerman@gmail.com>
2015-07-26 15:24:05 +0200

    Updated navigation entry form for dual language

--- viaduct/forms/navigation.py
+++ viaduct/forms/navigation.py
@@ -1,12 +1,19 @@
 from flask_wtf import Form
-from wtforms import StringField, BooleanField, SubmitField
+from wtforms import StringField, BooleanField
 from wtforms.validators import InputRequired
+from flask.ext.babel import lazy_gettext as _
 
 
 class NavigationEntryForm(Form):
-    title = StringField('Titel', validators=[InputRequired()])
-    parent_id = StringField('Lijst')
-    url = StringField('URL', validators=[InputRequired()])
-    external = BooleanField('Externe link', default=False)
-    activity_list = BooleanField('Lijst van activiteiten', default=False)
-    submit = SubmitField('Opslaan')
+    nl_title = StringField(_('Dutch title'), validators=[
+        InputRequired(_('Dutch title') + " " + ('is required'))
+    ])
+    en_title = StringField(_('English title'), validators=[
+        InputRequired(_('English title') + ' ' + ('is required'))
+    ])
+    url = StringField('URL', validators=[
+        InputRequired('URL ' + _('is required'))
+    ])
+    parent_id = StringField(_('List'))
+    external = BooleanField(_('External link'), default=False)
+    activity_list = BooleanField(_('List of activities'), default=False)
