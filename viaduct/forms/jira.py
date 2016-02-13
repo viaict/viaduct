2fad69a7c1eda0baf658d8f082aeb20671ce006d
Jelte Fennema <github-tech@jeltef.nl>
2016-02-13 15:09:25 +0100

    Translate bug report page

--- viaduct/forms/jira.py
+++ viaduct/forms/jira.py
@@ -2,23 +2,24 @@ from flask_wtf import Form
 from wtforms import StringField, SelectField, TextAreaField
 from wtforms.validators import InputRequired
 from werkzeug.datastructures import MultiDict
+from flask.ext.babel import lazy_gettext as _
 
 
 class CreateIssueForm(Form):
     summary = StringField(
-        'Titel',
+        _('Title'),
         validators=[
-            InputRequired(message='Geen titel opgegeven')]
+            InputRequired(message=_('No title entered'))]
     )
     issue_type = SelectField(
-        'Issue type',
+        _('Bug type'),
         choices=[('1', 'Bug'), ('2', 'New Feature'), ('4', 'Improvement')],
         default=1,
-        validators=[InputRequired(message='Geen Issue type opgegeven')]
+        validators=[InputRequired(message=_('No issue type supplied'))]
     )
     description = TextAreaField(
-        'Beschrijving',
-        validators=[InputRequired(message='Geen omschrijving opgegeven')]
+        _('Description'),
+        validators=[InputRequired(message=_('No description supplied'))]
     )
 
     def reset(self):
