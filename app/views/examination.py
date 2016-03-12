d5f33616c18dc534b7acb61c23fec99df2de6101
Jelte Fennema <github-tech@jeltef.nl>
2016-03-12 13:52:28 +0100

    Refactor: Import utils instead of utilities (and fix circular imports)

--- app/views/examination.py
+++ app/views/examination.py
@@ -12,7 +12,7 @@ from app import app, db
 from app.forms import CourseForm
 from app.utils import flash_form_errors
 from app.forms import EducationForm
-from app.utilities import serialize_sqla
+from app.utils import serialize_sqla
 
 from app.models.examination import Examination, test_types
 from app.models.course import Course
