9c741c735c58612715857a547926500113258cd2
Jelte Fennema <github-tech@jeltef.nl>
2016-04-09 15:33:12 +0200

    Test pre-receive hook

--- googletest.py
+++ googletest.py
@@ -1,6 +1,9 @@
 import app.api.google as google
 from pprint import pprint
 
+
+
+
 service = google.build_groups_service()
 groups_api = service.groups()
 pprint(google.create_group('ICT testmail list', 'icttest'))
