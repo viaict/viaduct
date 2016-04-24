23aa7bd1583dee2e741e2881b4d3bfcd0b5aeec8
Lorian Coltof <loriancoltof@gmail.com>
2016-04-24 10:14:02 +0200

    Added list of problems page and problem view routes

--- app/views/domjudge.py
+++ app/views/domjudge.py
@@ -1,9 +1,11 @@
 from flask import Blueprint
-from flask import flash, redirect, render_template, url_for, request
+from flask import flash, redirect, \
+    render_template, url_for, request, Response
 from flask.ext.login import login_required
 from flask.ext.babel import _
 
 from app import app
+# from flask.ext.login import current_user
 
 import requests
 import datetime as dt
@@ -30,6 +32,38 @@ def domjudge_request_get(url):
         return None
 
     return r
+
+
+def domjudge_request_post(url, data, session=None, flash_on_error=True):
+    if not session:
+        session = requests.Session()
+    try:
+        r = requests.post(DOMJUDGE_URL + url, data=data, timeout=(5, 5))
+    except:
+        if flash_on_error:
+            flash_error()
+        return None
+
+    if r.status_code >= 400:
+        if flash_on_error:
+            flash_error()
+        return None
+
+    return r, session
+
+
+def domjudge_login(username, password):
+    form_data = {
+        'cmd': 'login',
+        'login': username,
+        'passwd': password
+    }
+
+    res = domjudge_request_post('public/login.php', form_data,
+                                flash_on_error=False)
+    if res:
+        return res[1]
+    return None
 
 
 @blueprint.route('/')
@@ -106,9 +140,45 @@ def contest_view(contest_id=None):
                            problems=problems, teams=teams_dict)
 
 
-@blueprint.route('/contest/<int:contest_id>/submit/')
-@login_required
-def contest_submit(contest_id=None):
+@blueprint.route('/contest/<int:contest_id>/problems/')
+def contest_problems_list(contest_id):
     r = domjudge_request_get('api/contests')
     if not r:
-        return render_template('domjudge/list.htm')
+        return render_template('domjudge/problem/list.htm')
+
+    if str(contest_id) not in r.json():
+        flash(_("Contest does not exist"), 'danger')
+        return redirect(url_for('domjudge.contest_list'))
+
+    contest = r.json()[str(contest_id)]
+    contest['start'] = dt.datetime.fromtimestamp(contest['start']) \
+        .strftime(DT_FORMAT)
+    contest['end'] = dt.datetime.fromtimestamp(contest['end']) \
+        .strftime(DT_FORMAT)
+
+    r = domjudge_request_get('api/problems?cid={}'.format(contest_id))
+    if not r:
+        return render_template('domjudge/problem/list.htm')
+
+    problems = r.json()
+    problems.sort(key=lambda x: x['label'])
+
+    return render_template('domjudge/problem/list.htm',
+                           contest=contest, problems=problems)
+
+
+@blueprint.route('/problem/<int:problem_id>/')
+def contest_problem_view(problem_id):
+    r = domjudge_request_get('public/problem.php?id={}'.format(problem_id))
+    if not r:
+        return redirect(url_for('domjudge.contest_list'))
+
+    return Response(r.content, mimetype=r.headers['Content-Type'])
+
+
+@blueprint.route('/contest/<int:contest_id>/problem/<int:problem_id>/submit/',
+                 methods=['GET', 'POST'])
+@login_required
+def contest_problem_submit(contest_id, problem_id):
+    flash("Not implemented", 'danger')
+    return render_template('content.htm')
