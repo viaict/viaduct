0e795697e6c40c2820c86375416ab5e1b5fc4025
Braamling <contact@bramvandenakker.nl>
2016-05-21 14:41:21 +0200

    fixed pep8 atttempt

--- app/utils/seo.py
+++ app/utils/seo.py
@@ -10,7 +10,7 @@ class SeoAPI(Resource):
 
     @staticmethod
     def get_seo_fields(language='nl', module_name=None, request_path=None):
-        """ Get the seo fields as dict"""
+        """Get the seo fields as dict"""
         # Check if the module and path are set.
         if module_name is None:
             module_name = request.blueprint
