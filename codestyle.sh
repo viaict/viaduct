3189dbe3852ce6e23d11af6dfa98af9a8b4d63df
Maico Timmerman <maico.timmerman@gmail.com>
2016-06-18 21:05:02 +0200

    Fixed crash of js and py checks to ignore deleted git files.

--- codestyle.sh
+++ codestyle.sh
@@ -3,7 +3,6 @@
 # Exit on any error
 set -e
 
-
 # Default git params
 if [ $# -eq 0 ]; then
     gitparams='diff HEAD'
@@ -18,14 +17,13 @@ else
     flake8path=$2
 fi
 
-
 # Check python file changes for flake8 errors and ignore migrations since they
-# are auto generated
-git $gitparams --name-only | grep .py\$ | grep --invert-match '^migrations/versions/' | xargs --no-run-if-empty $flake8path
+# are auto generated, ignore deleted files.
+git $gitparams --name-status | grep ^[^D].*.py\$ | cut -d'	' -f2- | grep --invert-match '^migrations/versions/' | xargs --no-run-if-empty $flake8path
 
 # Check for JSHint errors in changed files except for minified files since
-# these are not created by us
-git $gitparams --name-only | grep .js\$ | grep --invert-match '\.min\.' | xargs --no-run-if-empty jshint
+# these are not created by us, ignore deleted files.
+git $gitparams --name-status | grep ^[^D].*.js\$ | cut -d'	' -f2- | grep --invert-match '\.min\.' | xargs --no-run-if-empty jshint
 
 # Fuzzy means translations are changed and probably incorrect
 ! grep fuzzy app/translations/nl/LC_MESSAGES/messages.po > /dev/null || \
