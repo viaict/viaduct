f02d015068fb7af22b70a4aa9927a68d28b199a8
Lorian Coltof <loriancoltof@gmail.com>
2016-06-29 14:34:22 +0200

    Fixed group creation without email address

--- app/views/group.py
+++ app/views/group.py
@@ -83,7 +83,11 @@ def create():
             valid_form = False
 
         if valid_form:
-            group = Group(name, maillist.lower())
+            maillist = maillist.strip().lower()
+            if maillist == '':
+                group = Group(name, None)
+            else:
+                group = Group(name, maillist)
 
             db.session.add(group)
             db.session.commit()
