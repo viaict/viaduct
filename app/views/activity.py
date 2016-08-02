06a982e8cd8b109ec5f77af7479decaf85785333
Roan de Jong <rolojong@hotmail.com>
2016-08-02 15:13:49 +0200

    Fixed reserve_list being able to pay bug

--- app/views/activity.py
+++ app/views/activity.py
@@ -105,13 +105,17 @@ def get_activity(activity_id=0):
         # Check if the current user has already entered data in this custom
         # form
         if current_user.is_authenticated:
-            form_result = CustomFormResult.query \
-                .filter(CustomFormResult.form_id == activity.form_id) \
+            all_form_results = CustomFormResult.query \
+                .filter(CustomFormResult.form_id == activity.form_id)
+            form_result = all_form_results \
+                .filter(CustomFormResult.owner_id == current_user.id).first()
+            attending = all_form_results.limit(activity.form.max_attendants) \
+                .from_self() \
                 .filter(CustomFormResult.owner_id == current_user.id).first()
 
             if form_result:
                 activity.form_data = form_result.data.replace('"', "'")
-                if not form_result.has_payed:
+                if not form_result.has_payed and attending:
                     # There is 50 cents administration fee
                     if form_result.form.price - 0.5 > 0:
                         form.show_pay_button = True
@@ -121,13 +125,17 @@ def get_activity(activity_id=0):
                     activity.info = _("Your registration has been completed")\
                         + _("You can edit your registration by resubmitting"
                             "the form.")
-                else:
+                elif attending:
                     activity.info = _("You have successfully registered"
                                       ", payment is still required!")
+                else:
+                    activity.info = _("The activity has reached its maximum "
+                                      "number of registrations. You have been "
+                                      "placed on the reserves list.")
             else:
                 if activity.num_attendants >= activity.form.max_attendants:
                     activity.info = _("The activity has reached its maximum "
-                                      "number of registrations. You have been "
+                                      "number of registrations. You will be "
                                       "placed on the reserves list.")
                 else:
                     activity.info = _("The number of registrations at this "
