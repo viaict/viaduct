24d85a01d5a56ec5e2c39ee451d9df22c6237ea6
Ben Wiersma <b.j.j.wiersma@gmail.com>
2016-09-10 12:14:46 +0200

    Add %s to translations so they can be compiled

--- codestyle.sh
+++ codestyle.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Exit on any error
-set -e
-
 # Default git params
 if [ $# -eq 0 ]; then
     gitparams='diff HEAD'
@@ -19,11 +16,20 @@ fi
 
 # Check python file changes for flake8 errors and ignore migrations since they
 # are auto generated, ignore deleted files.
-git $gitparams --name-status | grep ^[^D].*.py\$ | cut -d'	' -f2- | grep --invert-match '^migrations/versions/' | xargs --no-run-if-empty $flake8path
+py_files=$(git $gitparams --name-status | grep ^[^D].*.py\$ | cut -d'	' -f2- | grep --invert-match '^migrations/versions/')
+if [[ $? -ne 0 ]]; then
+  echo $py_files | xargs $flake8path
+fi
 
 # Check for JSHint errors in changed files except for minified files since
 # these are not created by us, ignore deleted files.
-git $gitparams --name-status | grep ^[^D].*.js\$ | cut -d'	' -f2- | grep --invert-match '\.min\.' | xargs --no-run-if-empty jshint
+js_files=$(git $gitparams --name-status | grep ^[^D].*.js\$ | cut -d'	' -f2- | grep --invert-match '\.min\.')
+if [[ $? -ne 0 ]]; then
+  echo $js_files | xargs jshint
+fi
+
+# Exit on any error
+set -e
 
 # Fuzzy means translations are changed and probably incorrect
 ! grep fuzzy app/translations/nl/LC_MESSAGES/messages.po > /dev/null || \
