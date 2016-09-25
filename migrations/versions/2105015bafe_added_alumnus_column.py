ecba1acb94e0c06e2a62d027533248cf91e5cfff
Roan de Jong <rolojong@hotmail.com>
2016-09-25 11:38:32 +0200

    updated user table

--- migrations/versions/2105015bafe_added_alumnus_column.py
+++ migrations/versions/2105015bafe_added_alumnus_column.py
@@ -1,14 +1,14 @@
-"""Added alumnus column
+"""added_alumnus_column
 
-Revision ID: 41dd3272eb9
-Revises: 2d257d93329
-Create Date: 2016-08-10 18:29:03.035712
+Revision ID: 2105015bafe
+Revises: 404db3c1f74
+Create Date: 2016-09-10 18:20:33.935231
 
 """
 
 # revision identifiers, used by Alembic.
-revision = '41dd3272eb9'
-down_revision = '2d257d93329'
+revision = '2105015bafe'
+down_revision = '404db3c1f74'
 
 from alembic import op
 import sqlalchemy as sa
