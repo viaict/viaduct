59ebfb307c98b17bd35623a7369dedc475133e41
Maico Timmerman <maico.timmerman@gmail.com>
2016-10-01 14:10:39 +0200

    Moved all the scripts to be included in manage.py later

--- cleanup.py
+++
@@ -1,8 +0,0 @@
-import os
-
-for current, directories, files in os.walk(os.getcwd()):
-    for file in files:
-        path = os.path.join(current, file)
-
-        if os.path.splitext(path)[1] == '.bak':
-            os.remove(path)
--- run.wsgi
+++
@@ -1,7 +0,0 @@
-import logging
-import sys
-
-logging.basicConfig(stream=sys.stderr)
-
-from app import application
-
