3274a9edf346d82921e5883f239b97707b79692c
Roan de Jong <rolojong@hotmail.com>
2016-11-13 11:18:49 +0100

    VIA-1611: Written data migration to separate mailing lists from User table

--- app/models/mailing_lists.py
+++ app/models/mailing_lists.py
@@ -5,9 +5,9 @@ from app.models import BaseEntity
 class MailingList(db.Model, BaseEntity):
 
     name = db.Column(db.String(64))  # display name
-    copernica_db_id = db.Column(db.Integer)
-    active = db.column(db.Boolean)
-    member_only = db.column(db.Boolean)
+    copernica_db_id = db.Column(db.Integer())
+    active = db.column(db.Boolean())
+    member_only = db.column(db.Boolean())
 
     def __init__(self, name="", db_id=0,
                  active=False, member_only=True):
@@ -21,17 +21,17 @@ class MailingListUser(db.Model, BaseEntity):
 
     prints = ('user_id', 'mailing_list_id', 'copernica_id', 'subscribed')
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
     user = db.relationship('User',
                            backref=db.backref('mailinglists',
                                               lazy='dynamic'))
-    mailing_list_id = db.Column(db.Integer,
+    mailing_list_id = db.Column(db.Integer(),
                                 db.ForeignKey('mailing_list.copernica_db_id'))
     mailing_list = db.relationship('MailingList',
                                    backref=db.backref('followers',
                                                       lazy='dynamic'))
-    copernica_user_id = db.Column(db.Integer)
-    subscribed = db.Column(db.Boolean, default=False)
+    copernica_user_id = db.Column(db.Integer())
+    subscribed = db.Column(db.Boolean(), default=False)
 
     def __init__(self, user_id=0, mailing_list_id=0,
                  copernica_id=0, subscribed=False):
