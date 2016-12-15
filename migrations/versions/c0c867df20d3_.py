2bc19089d6e202591161d769a2457e06acc43156
Maico Timmerman <maico.timmerman@gmail.com>
2016-12-15 11:40:25 +0100

    Added merge migration

---
+++ migrations/versions/c0c867df20d3_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: c0c867df20d3
+Revises: ('3f117d44226', 'fb7167c99f57')
+Create Date: 2016-11-28 23:12:21.813100
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c0c867df20d3'
+down_revision = ('3f117d44226', 'fb7167c99f57')
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
