37c0b8dc8bef12632ed9bd94ff83ce45628c29f4
Florens Douwes <florens.douwes@student.uva.nl>
2017-06-10 14:04:50 +0200

    Make the price input for activities and forms more clear.

--- app/forms/custom_form.py
+++ app/forms/custom_form.py
@@ -1,7 +1,8 @@
-from flask_wtf import Form
 from flask_babel import lazy_gettext as _
+from flask_wtf import Form
 from wtforms import StringField, SelectField
 from wtforms.validators import InputRequired, Email
+
 from app.forms.fields import DecimalField
 
 
@@ -16,6 +17,7 @@ class CreateForm(Form):
     first_name = StringField('Voornaam', validators=[InputRequired()])
     last_name = StringField('Achternaam', validators=[InputRequired()])
     student_id = StringField('Student ID', validators=[InputRequired()])
-    price = DecimalField(_('Price'), places=2, validators=[InputRequired()])
+    price = DecimalField(_('IDeal price'), places=2,
+                         validators=[InputRequired()])
     education_id = SelectField('Opleiding', coerce=int)
     terms = StringField(_('Conditions'))
