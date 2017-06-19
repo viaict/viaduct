e7fed47935e86090b9f4415832c2d03853e2191d
Sijmen Schoon <me@sijmenschoon.nl>
2017-06-19 21:41:35 +0200

    Filter archived news items

--- app/forms/newsletter.py
+++ app/forms/newsletter.py
@@ -2,6 +2,7 @@ import datetime
 from flask_babel import lazy_gettext as _
 from flask_wtf import Form
 from wtforms.ext.sqlalchemy.fields import QuerySelectMultipleField
+from app import db
 from app.models.activity import Activity
 from app.models.news import News
 
@@ -9,8 +10,10 @@ from app.models.news import News
 def news_factory():
     start_date = datetime.date.today().replace(day=1)
     prev_month = (start_date - datetime.timedelta(days=1)).replace(day=1)
-    return News.query.filter(News.created > prev_month)\
-                     .order_by(News.created).all()
+    return News.query.filter(News.created > prev_month,
+                             db.or_(News.archive_date >= datetime.date.today(),
+                                    News.archive_date == None))\
+                     .order_by(News.created).all()  # noqa
 
 
 def activities_factory():
