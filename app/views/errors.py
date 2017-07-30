d2f30e13fb72144803e36ad8b06c5df38ea3504e
Maico Timmerman <maico.timmerman@gmail.com>
2017-07-30 20:46:26 +0200

    Correctly handle ajax request in errors.

--- app/views/errors.py
+++ app/views/errors.py
@@ -1,13 +1,12 @@
+import re
+
 from flask import flash, request, url_for, render_template, redirect, \
-    session
-from flask_login import current_user
+    session, jsonify
 from flask_babel import _
+from flask_login import current_user
 
 from app import app, login_manager
 from app.models import Page
-
-
-import re
 
 
 @login_manager.unauthorized_handler
@@ -22,6 +21,9 @@ def unauthorized():
 @app.errorhandler(403)
 def permission_denied(e):
     """When permission denied and not logged in you will be redirected."""
+    if request.is_xhr:
+        return jsonify('You are not allowed to access this resource'), 403
+
     content = "403, The police has been notified!"
     image = '/static/img/403.jpg'
 
@@ -37,11 +39,16 @@ def permission_denied(e):
 
 @app.errorhandler(500)
 def internal_server_error(e):
+    if request.is_xhr:
+        return jsonify('An internal server error occurred'), 500
     return render_template('page/500.htm'), 500
 
 
 @app.errorhandler(404)
 def page_not_found(e):
+    if request.is_xhr:
+        return jsonify('The requested resource could not be found.'), 404
+
     # Search for file extension.
     if re.match(r'(?:.*)\.[a-zA-Z]{3,}$', request.path):
         return '', 404
