5dff3117ccdb7ff4629889df6041aad02dc9298d
Maico Timmerman <maico.timmerman@gmail.com>
2017-08-10 19:44:39 +0200

    Made mysqlinit work for docker by enabling '%' host

--- script/mysqlinit.sql
+++ script/mysqlinit.sql
@@ -1,2 +1,2 @@
-create user 'viaduct'@'localhost' identified by 'viaduct';
-grant all privileges on viaduct.* to 'viaduct'@'localhost';
+create user 'viaduct'@'%' identified by 'viaduct';
+grant all privileges on viaduct.* to 'viaduct'@'%';
