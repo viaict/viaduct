64c2dc8b5ed6190952320e1b244ab29b075c744a
Lorian Coltof <loriancoltof@gmail.com>
2017-08-15 15:20:20 +0200

    Merge DB migrations

---
+++ migrations/versions/e28438a52376_.py
@@ -0,0 +1,22 @@
+"""empty message.
+
+Revision ID: e28438a52376
+Revises: ('f1ad9b0e2c1e', 'd3e9983f8366')
+Create Date: 2017-08-15 15:19:45.458758
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e28438a52376'
+down_revision = ('f1ad9b0e2c1e', 'd3e9983f8366')
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
