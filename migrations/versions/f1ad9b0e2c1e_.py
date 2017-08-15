4f6bf49ffc8cbeff9af5e72d84ddbc0bb96240e5
Maico Timmerman <maico.timmerman@gmail.com>
2017-08-15 14:38:31 +0200

    Fixed database migration merge

---
+++ migrations/versions/f1ad9b0e2c1e_.py
@@ -0,0 +1,22 @@
+"""empty message.
+
+Revision ID: f1ad9b0e2c1e
+Revises: ('501bbf4c4770', '6cc5e07a4da9')
+Create Date: 2017-08-15 14:38:11.035577
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f1ad9b0e2c1e'
+down_revision = ('501bbf4c4770', '6cc5e07a4da9')
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
