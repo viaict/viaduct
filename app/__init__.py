36a2346f8f3cb93ed495a9c96233fd29f2193cae
Maico Timmerman <maico.timmerman@gmail.com>
2017-08-27 21:57:38 +0200

    Version bump

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ from app.utils.import_module import import_module
 from .extensions import db, login_manager, \
     cache, toolbar, jsglue, sentry
 
-version = 'v2.8.1.2'
+version = 'v2.8.2.0'
 
 
 def static_url(url):
