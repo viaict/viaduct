93af50e37b9b30e91dbd4de25dd149bb6ef2434a
Sijmen Schoon <me@sijmenschoon.nl>
2017-11-20 21:44:57 +0100

    Add Docker support for all three environments

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
 FROM python:3.6
 
-RUN apt-get update && apt-get install -y
+RUN apt-get update && apt-get install -y node npm
+RUN pip install uwsgi
 
 WORKDIR /app
-ADD requirements.txt /app
+
+COPY package.json /app
+RUN npm install
+
+COPY requirements.txt /app
 RUN pip install -r requirements.txt
-RUN pip install uwsgi
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
 # Exit on error
 set -e
 
 cd /app
 
+rm -f config.py
+if [ $ENVIRONMENT = "prod" ]; then
+	ln -s 'secrets/master.py' 'config.py'
+elif [ $ENVIRONMENT = "dev" ]; then
+	ln -s 'secrets/develop.py' 'config.py'
+else
+	ln -s 'secrets/local.py' 'config.py'
+fi
+
 echo 'Applying database migrations...'
 python manage.py db upgrade
 
 echo 'Starting server'
-uwsgi --ini viaduct-master.ini
+if [ $ENVIRONMENT = 'prod' -o $ENVIRONMENT = 'dev' ]; then
+	uwsgi --ini viaduct-master.ini
+else
+	./watch.sh
+fi
