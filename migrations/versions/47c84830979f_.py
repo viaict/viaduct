71bb1a57a509f6fc0ff8f8642382715994166cec
Ben Wiersma <b.j.j.wiersma@gmail.com>
2017-12-09 12:49:06 +0100

    Merge heads to make db upgradeable again

---
+++ migrations/versions/47c84830979f_.py
@@ -0,0 +1,22 @@
+"""empty message.
+
+Revision ID: 47c84830979f
+Revises: ('760cc9ca5d24', 'a419497ed341')
+Create Date: 2017-12-09 12:48:17.680962
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '47c84830979f'
+down_revision = ('760cc9ca5d24', 'a419497ed341')
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
