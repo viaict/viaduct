95da125fb8c7b1a393d0f4ca3285ac6a173c7042
Maico Timmerman <maico.timmerman@gmail.com>
2017-12-28 15:05:43 +0100

    Added database migration merge

---
+++ migrations/versions/2de19b331017_.py
@@ -0,0 +1,22 @@
+"""empty message.
+
+Revision ID: 2de19b331017
+Revises: ('47c84830979f', 'cadae0cd7780')
+Create Date: 2017-12-28 15:05:30.217419
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2de19b331017'
+down_revision = ('47c84830979f', 'cadae0cd7780')
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
