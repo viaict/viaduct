fe7b9ddb580adb76784da8c34f8e4fcc387beed6
Florens <florens@Florenss-iMac.lan>
2018-01-09 23:48:59 +0100

    task_repository: use db.query

--- app/repository/task_repository.py
+++ app/repository/task_repository.py
@@ -1,8 +1,9 @@
+from app import db
 from app.models.pimpy import Task
 
 
 def find_task_by_name_content_group(name, content, group):
-    return Task.query.filter(
+    return db.query(Task).filter(
         Task.title == name,
         Task.content == content,
         Task.group_id == group.id).first()
