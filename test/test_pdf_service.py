e16d4551db08960189741d784f136341f21865ef
Boas Kluiving <boaskluiving@gmail.com>
2018-01-13 17:35:03 +0100

    Minor improvenments in pdf unittest

--- test/test_pdf_service.py
+++ test/test_pdf_service.py
@@ -9,9 +9,6 @@ from app.models.user import User
 @patch.object(pdf_service, "user_service", autospec=True)
 class TestPDFService(unittest.TestCase):
 
-    def setUp(self):
-        pass
-
     def test_generate_pdf(self, user_service_mock):
 
         # Create the mock user to generate the pdf of
@@ -23,6 +20,7 @@ class TestPDFService(unittest.TestCase):
         user_service_mock.find_by_id.return_value = mock_user
 
         pdf_bytes = pdf_service.user_discount_card(1)
+        user_service_mock.find_by_id.assert_called_once_with(1)
 
         # Check if all the user info appears in the generated pdf
         self.assertTrue(bytes(mock_user.first_name, "utf-8") in pdf_bytes)
