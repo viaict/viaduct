d0b7d2f42e3a608ae26e62682f2b4a542412a699
Maico Timmerman <maico.timmerman@gmail.com>
2018-05-08 00:25:38 +0200

    Removed duplicate assert calls.

--- test/test_alv_service.py
+++ test/test_alv_service.py
@@ -2,11 +2,10 @@ import unittest
 from unittest import mock
 from unittest.mock import patch
 
-
-from app.service import alv_service
+from app.enums import FileCategory
 from app.exceptions import ResourceNotFoundException
 from app.models.alv_model import Alv
-from app.enums import FileCategory
+from app.service import alv_service
 
 alv_repository_mock = mock.MagicMock()
 file_service_mock = mock.MagicMock()
@@ -25,8 +24,7 @@ class TestAlvService(unittest.TestCase):
 
         alv_service.save_alv(alv)
 
-        alv_repository_mock.save.assert_called_with(alv)
-        alv_repository_mock.save.assert_called_once()
+        alv_repository_mock.save.assert_called_once_with(alv)
 
     def test_find_all_alv(self):
         alv_service.find_all_alv()
@@ -35,30 +33,27 @@ class TestAlvService(unittest.TestCase):
     def test_find_alv_by_id(self):
         alv_service.find_alv_by_id(1, False, False)
 
-        alv_repository_mock.find_alv_by_id.assert_called_with(
+        alv_repository_mock.find_alv_by_id.assert_called_once_with(
             1, include_presidium=False, include_documents=False)
 
     def test_get_alv_by_id(self):
 
         alv_service.get_alv_by_id(1)
 
-        alv_repository_mock.find_alv_by_id.assert_called_with(
+        alv_repository_mock.find_alv_by_id.assert_called_once_with(
             1, include_presidium=True, include_documents=False)
-        alv_repository_mock.find_alv_by_id.assert_called_once()
 
     def test_find_alv_document_by_id(self):
         alv_service.find_alv_document_by_id(1, True)
 
-        alv_repository_mock.find_alv_document_by_id.assert_called_with(
-            1, True)
-        alv_repository_mock.find_alv_document_by_id.assert_called_once()
+        alv_repository_mock.find_alv_document_by_id. \
+            assert_called_once_with(1, True)
 
     def test_get_alv_document_by_id(self):
         alv_service.get_alv_document_by_id(1)
 
-        alv_repository_mock.find_alv_document_by_id.assert_called_with(
-            1, False)
-        alv_repository_mock.find_alv_document_by_id.assert_called_once()
+        alv_repository_mock.find_alv_document_by_id. \
+            assert_called_once_with(1, False)
 
     def test_get_alv_document_by_id_not_found(self):
         alv_repository_mock.find_alv_document_by_id.return_value = None
@@ -66,9 +61,8 @@ class TestAlvService(unittest.TestCase):
         with self.assertRaises(ResourceNotFoundException):
             alv_service.get_alv_document_by_id(1)
 
-        alv_repository_mock.find_alv_document_by_id.assert_called_with(
-            1, False)
-        alv_repository_mock.find_alv_document_by_id.assert_called_once()
+        alv_repository_mock.find_alv_document_by_id. \
+            assert_called_once_with(1, False)
 
     def test_add_minutes(self):
         alv = mock.MagicMock(spec=dir(Alv))
@@ -87,8 +81,7 @@ class TestAlvService(unittest.TestCase):
 
         alv_service.add_document(alv, file_storage, "nl", "en")
 
-        alv_repository_mock.create_document.assert_called_with()
-        alv_repository_mock.create_document.assert_called_once()
+        alv_repository_mock.create_document.assert_called_once_with()
         alv_repository_mock.save_document_version.assert_called_once()
         alv_repository_mock.save_document.assert_called_once()
         file_service_mock.add_file.assert_called_with(
@@ -106,9 +99,8 @@ class TestAlvService(unittest.TestCase):
         with self.assertRaises(ResourceNotFoundException):
             alv_service.get_alv_by_id(1)
 
-        alv_repository_mock.find_alv_by_id.assert_called_with(
+        alv_repository_mock.find_alv_by_id.assert_called_once_with(
             1, include_presidium=True, include_documents=False)
-        alv_repository_mock.find_alv_by_id.assert_called_once()
 
     def test_update_document(self):
         doc = mock.MagicMock()
@@ -141,5 +133,4 @@ class TestAlvService(unittest.TestCase):
 
         alv_service.delete_alv(alv)
 
-        alv_repository_mock.delete_alv.assert_called_with(alv)
-        alv_repository_mock.delete_alv.assert_called_once()
+        alv_repository_mock.delete_alv.assert_called_once_with(alv)
